ea29ad39df4b47e162aaa7914232c609f712bb7a
vladiuse <vladiuse@gmail.com>
2025-07-09 22:09:59 +0300

    init

---
+++ app/asana/views.py
@@ -0,0 +1,22 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import AsanaWebhookRequestData
+
+
+@api_view(http_method_names=["GET", "POST"])
+def webhook(request, format=None):
+    secret = request.headers.get("X-Hook-Secret")
+    if not secret:
+        return Response(data={"error": '"X-Hook-Secret" header required'}, status=status.HTTP_400_BAD_REQUEST)
+    AsanaWebhookRequestData.objects.create(
+        headers=dict(request.headers),
+        payload=dict(request.data),
+    )
+    data = {
+        "success": True,
+        "method": request.method,
+        "headers": request.headers,
+    }
+    return Response(data=data)
