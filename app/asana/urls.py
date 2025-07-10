e80e147ee0ea59fca8dd2ea9d08f8d24b89b9ab4
vladiuse <vladiuse@gmail.com>
2025-07-10 14:43:04 +0300

    add serializers

--- app/asana/urls.py
+++ app/asana/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+router = DefaultRouter()
+router.register("asana-webhooks", views.AsanaWebhookRequestDataView)
+
+
 urlpatterns = [
     path("webhook/", views.webhook),
+    path("", include(router.urls)),
 ]
--- app/asana/views.py
+++ app/asana/views.py
@@ -1,11 +1,12 @@
 from django.conf import settings
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from .models import AsanaWebhookRequestData
+from .serializers import AsanaWebhookRequestDataSerializer
 
-
-@api_view(http_method_names=["GET", "POST"])
+@api_view(http_method_names=["POST"])
 def webhook(request, format=None):
     secret = request.headers.get("X-Hook-Secret")
     # if not secret:
@@ -22,3 +23,19 @@ def webhook(request, format=None):
     response = Response(data=data)
     response["X-Hook-Secret"] = settings.ASANA_HOOK_SECRET if settings.ASANA_HOOK_SECRET else secret
     return response
+
+
+class AsanaWebhookRequestDataView(ModelViewSet):
+    queryset = AsanaWebhookRequestData.objects.order_by("-pk")
+    serializer_class = AsanaWebhookRequestDataSerializer
+
+
+    @action(detail=True)
+    def headers(self, request, pk):
+        asana_webhook = self.get_object()
+        return Response(data=asana_webhook.headers)
+
+    @action(detail=True)
+    def payload(self, request, pk):
+        asana_webhook = self.get_object()
+        return Response(data=asana_webhook.payload)
