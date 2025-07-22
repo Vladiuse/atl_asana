b72cdb15f26077e5a67f90de3aee0ccfc09118de
vladiuse <vladiuse@gmail.com>
2025-07-22 19:14:27 +0300

    update asana app

--- app/asana/views.py
+++ app/asana/views.py
@@ -1,24 +1,26 @@
 from django.conf import settings
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import AsanaWebhookRequestData
 from .serializers import AsanaWebhookRequestDataSerializer
+from .services import completed_task_creator
+
 
 @api_view(http_method_names=["POST"])
 def webhook(request, format=None):
     secret = request.headers.get("X-Hook-Secret")
-    # if not secret:
-    #     return Response(data={"error": '"X-Hook-Secret" header required'}, status=status.HTTP_400_BAD_REQUEST)
-    AsanaWebhookRequestData.objects.create(
+    asana_webhook = AsanaWebhookRequestData.objects.create(
         headers=dict(request.headers),
         payload=dict(request.data),
     )
+    created = completed_task_creator(asana_webhook_model=asana_webhook)
     data = {
         "success": True,
         "method": request.method,
         "headers": request.headers,
+        "created": len(created),
     }
     response = Response(data=data)
     response["X-Hook-Secret"] = settings.ASANA_HOOK_SECRET if settings.ASANA_HOOK_SECRET else secret
@@ -28,7 +30,6 @@ def webhook(request, format=None):
 class AsanaWebhookRequestDataView(ModelViewSet):
     queryset = AsanaWebhookRequestData.objects.order_by("-pk")
     serializer_class = AsanaWebhookRequestDataSerializer
-
 
     @action(detail=True)
     def headers(self, request, pk):
