09ec40adc62210c0dbce7af250bf7557b1a76967
vladiuse <vladiuse@gmail.com>
2025-11-05 18:24:41 +0300

    add admin dispaly

--- app/asana/admin.py
+++ app/asana/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import AtlasUser
+
+
+class AtlasUserAdmin(admin.ModelAdmin):
+    list_display = ["user_id", "atlas_profile_id", "email", "name", "avatar_preview"]
+
+    def avatar_preview(self, obj) -> str:
+        if not obj.avatar_url:
+            return ""
+        return format_html(
+            '<img src="{}" width="60" height="60" style="border-radius:50%; object-fit:cover;" />',
+            obj.avatar_url,
+        )
+
+    avatar_preview.short_description = "Avatar"
+
+
+admin.site.register(AtlasUser, AtlasUserAdmin)
