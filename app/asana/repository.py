3ca1bf72a3fa1c6736c028c7ea7f209d3cf311eb
vladiuse <vladiuse@gmail.com>
2025-11-05 18:24:13 +0300

    add repository

---
+++ app/asana/repository.py
@@ -0,0 +1,42 @@
+import logging
+from typing import Any
+
+from asana.client import AsanaApiClient
+
+from .constants import ATLAS_WORKSPACE_ID
+from .models import AtlasUser
+
+
+class AsanaUserRepository:
+    def __init__(self, api_client: AsanaApiClient):
+        self.api_client = api_client
+
+    def get_user(self, filters: dict[str, Any]) -> AtlasUser:
+        try:
+            return AtlasUser.objects.get(**filters)
+        except AtlasUser.DoesNotExist:
+            pass
+
+    def update_all(self) -> None:
+        atlas_memberships = self.api_client.get_workspace_memberships_for_workspace(workspace_id=ATLAS_WORKSPACE_ID)
+        logging.info("Memberships in asana: %s", len(atlas_memberships))
+        exist_memberships_in_db = [str(i) for i in AtlasUser.objects.values_list("atlas_profile_id", flat=True)]
+        logging.info("Exists memberships ids: %s", exist_memberships_in_db)
+        logging.info("Memberships in DB: %s", len(exist_memberships_in_db))
+        for membership in atlas_memberships:
+            if membership["gid"] not in exist_memberships_in_db:
+                atlas_profile_id = membership["gid"]
+                logging.info("Detect new Memberships: %s", atlas_profile_id)
+                name = membership["user"]["name"]
+                user_id = membership["user"]["gid"]
+                user_data = self.api_client.get_user(user_id=user_id)
+                email = user_data.get("email") or ""
+                photo = user_data["photo"].get("image_128x128", "") if user_data["photo"] else ""
+                AtlasUser.objects.create(
+                    atlas_profile_id=atlas_profile_id,
+                    name=name,
+                    user_id=user_id,
+                    avatar_url=photo,
+                    email=email,
+                )
+
