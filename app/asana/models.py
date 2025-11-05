de8f48029fb3f577df895c7300ac4bf08627501c
vladiuse <vladiuse@gmail.com>
2025-11-05 18:25:04 +0300

    fix model fields constraints

--- app/asana/models.py
+++ app/asana/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class AtlasUser(models.Model):
     user_id = models.PositiveIntegerField(unique=True)
     atlas_profile_id = models.PositiveIntegerField(unique=True)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(blank=True)
     name = models.CharField(max_length=254)
     avatar_url = models.URLField(blank=True)
