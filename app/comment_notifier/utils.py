2e611a85a6b9ee7a4d93db60ff73af2bd1533d02
vladiuse <vladiuse@gmail.com>
2025-11-06 15:46:33 +0300

    add utils extract_user_profile_id_from_text

---
+++ app/comment_notifier/utils.py
@@ -0,0 +1,8 @@
+import re
+
+USER_PROFILE_LINK_PATTERN = re.compile(r"https://app\.asana\.com/\d+/profile/(\d+)")
+
+
+def extract_user_profile_id_from_text(text: str) -> list[int]:
+    matches = re.findall(USER_PROFILE_LINK_PATTERN, text)
+    return [int(m) for m in matches]
