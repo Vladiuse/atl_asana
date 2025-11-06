c59b2aee907463569885d867de77ebea711c286f
vladiuse <vladiuse@gmail.com>
2025-11-06 13:48:55 +0300

    add field position

--- app/asana/models.py
+++ app/asana/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
 
+class Position(models.TextChoices):
+    BUYER = "buyer", "Баер"
+    FARMER = "farmer", "Фармер"
+
+
 class AtlasUser(models.Model):
     user_id = models.PositiveIntegerField(unique=True)
     atlas_profile_id = models.PositiveIntegerField(unique=True)
     email = models.EmailField(blank=True)
     name = models.CharField(max_length=254)
     avatar_url = models.URLField(blank=True)
+    position = models.CharField(
+        blank=True,
+        max_length=32,
+        choices=Position.choices,
+        verbose_name="Позиция",
+    )
