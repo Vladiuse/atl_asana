1a78938dc245499f4518135def7a13ab5287eee7
vladiuse <vladiuse@gmail.com>
2025-11-06 17:24:56 +0300

    add ProcessAsanaNewCommentEvent

--- app/comment_notifier/services.py
+++ app/comment_notifier/services.py
@@ -1,20 +1,40 @@
 from dataclasses import dataclass
 
-
-@dataclass
-class AsanaNewCommentEvent:
-    comment_id: int
-    user_id: int
-    task_id: int
+from .models import AsanaComment
 
 
-def process_asana_comment_event(events_data: dict) -> list[AsanaNewCommentEvent]:
-    result = []
-    for event in events_data["events"]:
-        event_dto = AsanaNewCommentEvent(
-            comment_id=event["resource"]["gid"],
-            user_id=event["user"]["gid"],
-            task_id=event["parent"]["gid"],
+class ProcessAsanaNewCommentEvent:
+    @dataclass
+    class AsanaNewCommentEvent:
+        comment_id: int
+        user_id: int
+        task_id: int
+
+    @dataclass
+    class Result:
+        created_comments_count: int
+        comments: list["ProcessAsanaNewCommentEvent.AsanaNewCommentEvent"]
+
+    def _event_to_comment_dto(self, events_data: dict) -> list[AsanaNewCommentEvent]:
+        result = []
+        for event in events_data["events"]:
+            event_dto = ProcessAsanaNewCommentEvent.AsanaNewCommentEvent(
+                comment_id=event["resource"]["gid"],
+                user_id=event["user"]["gid"],
+                task_id=event["parent"]["gid"],
+            )
+            result.append(event_dto)
+        return result
+
+    def process(self, events_data: dict) -> Result:
+        asana_comments_dto = self._event_to_comment_dto(events_data=events_data)
+        for comment_dto in asana_comments_dto:
+            AsanaComment.objects.create(
+                comment_id=comment_dto.comment_id,
+                user_id=comment_dto.user_id,
+                task_id=comment_dto.task_id,
+            )
+        return ProcessAsanaNewCommentEvent.Result(
+            created_comments_count=len(asana_comments_dto),
+            comments=asana_comments_dto,
         )
-        result.append(event_dto)
-    return result
