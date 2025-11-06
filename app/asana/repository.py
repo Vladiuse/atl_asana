abf4a79b93d2307af9c34e09370e51dc964e7452
vladiuse <vladiuse@gmail.com>
2025-11-06 16:59:54 +0300

    update repository

--- app/asana/repository.py
+++ app/asana/repository.py
@@ -10,6 +10,20 @@ class AsanaUserRepository:
     def __init__(self, api_client: AsanaApiClient):
         self.api_client = api_client
 
+    def _create_user_by_data(self, *, membership_data: dict, user_data: dict) -> AtlasUser:
+        user_id = user_data["gid"]
+        membership_id = membership_data["gid"]
+        name = membership_data["user"]["name"]
+        email = user_data.get("email") or ""
+        photo = user_data["photo"].get("image_128x128", "") if user_data["photo"] else ""
+        return AtlasUser.objects.create(
+            membership_id=membership_id,
+            name=name,
+            user_id=user_id,
+            avatar_url=photo,
+            email=email,
+        )
+
     def get_user(self, membership_id: int) -> AtlasUser:
         try:
             user = AtlasUser.objects.get(membership_id=membership_id)
@@ -20,43 +34,24 @@ class AsanaUserRepository:
             return self.create_by_membership_id(membership_id=membership_id)
 
     def update_all(self) -> None:
-        atlas_memberships = self.api_client.get_workspace_memberships_for_workspace(workspace_id=ATLAS_WORKSPACE_ID)
-        logging.info("Memberships in asana: %s", len(atlas_memberships))
-        exist_memberships_in_db = [str(i) for i in AtlasUser.objects.values_list("atlas_profile_id", flat=True)]
-        logging.info("Exists memberships ids: %s", exist_memberships_in_db)
+        atlas_asana_memberships = self.api_client.get_workspace_memberships_for_workspace(workspace_id=ATLAS_WORKSPACE_ID)
+        logging.info("Memberships in asana: %s", len(atlas_asana_memberships))
+        exist_memberships_in_db = [str(i) for i in AtlasUser.objects.values_list("membership_id", flat=True)]
         logging.info("Memberships in DB: %s", len(exist_memberships_in_db))
-        for membership in atlas_memberships:
-            if membership["gid"] not in exist_memberships_in_db:
-                atlas_profile_id = membership["gid"]
-                logging.info("Detect new Memberships: %s", atlas_profile_id)
-                name = membership["user"]["name"]
-                user_id = membership["user"]["gid"]
-                user_data = self.api_client.get_user(user_id=user_id)
-                email = user_data.get("email") or ""
-                photo = user_data["photo"].get("image_128x128", "") if user_data["photo"] else ""
-                AtlasUser.objects.create(
-                    atlas_profile_id=atlas_profile_id,
-                    name=name,
-                    user_id=user_id,
-                    avatar_url=photo,
-                    email=email,
-                )
+        new_created = 0
+        for membership_data in atlas_asana_memberships:
+            membership_id = membership_data["gid"]
+            if membership_data["gid"] not in exist_memberships_in_db:
+                logging.info("Detect new Memberships: %s", membership_id)
+                user_data = self.api_client.get_user(user_id=membership_data["user"]["gid"])
+                self._create_user_by_data(membership_data=membership_data, user_data=user_data)
+                new_created+= 1
+        logging.info("New created: %s", new_created)
 
     def create_by_membership_id(self, membership_id: int) -> AtlasUser:
-        membership = self.api_client.get_workspace_membership(membership_id=membership_id)
-        user_id = membership["user"]["gid"]
-        user_data = self.api_client.get_user(user_id=user_id)
-        atlas_profile_id = membership["gid"]
-        name = membership["user"]["name"]
-        email = user_data.get("email") or ""
-        photo = user_data["photo"].get("image_128x128", "") if user_data["photo"] else ""
-        return AtlasUser.objects.create(
-            atlas_profile_id=atlas_profile_id,
-            name=name,
-            user_id=user_id,
-            avatar_url=photo,
-            email=email,
-        )
+        membership_data = self.api_client.get_workspace_membership(membership_id=membership_id)
+        user_data = self.api_client.get_user(user_id=membership_data["user"]["gid"])
+        return self._create_user_by_data(membership_data=membership_data, user_data=user_data)
 
 
 
--- app/asana/scripts/asana/asana_users.py
+++ app/asana/scripts/asana/asana_users.py
@@ -10,6 +10,6 @@ asana_client = AsanaApiClient(api_key=settings.ASANA_API_KEY)
 repository = AsanaUserRepository(api_client=asana_client)
 logging.basicConfig(level=logging.INFO)
 def run() -> None:
-    # repository.update_all()
-    repository.create_by_membership_id(profile_id=1210393628043137)
+    repository.update_all()
+    # repository.create_by_membership_id(membership_id=1210393628043137)
 
