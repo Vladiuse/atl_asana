0351b034adf01477da5f0cb483cdf193324e7f83
vladiuse <vladiuse@gmail.com>
2025-11-06 13:39:50 +0300

    add script

---
+++ app/asana/scripts/asana/asana_users.py
@@ -0,0 +1,15 @@
+from pprint import pprint
+import logging
+import json
+from django.conf import settings
+from asana.client import AsanaApiClient
+from asana.constants import ATLAS_WORKSPACE_ID
+from asana.repository import AsanaUserRepository
+
+asana_client = AsanaApiClient(api_key=settings.ASANA_API_KEY)
+repository = AsanaUserRepository(api_client=asana_client)
+logging.basicConfig(level=logging.INFO)
+def run() -> None:
+    repository.update_all()
+
+
