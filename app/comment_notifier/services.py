8524e9352b63867aa4cf7ec5258b5704a7ef86ef
vladiuse <vladiuse@gmail.com>
2025-11-07 15:09:17 +0300

    fix ProcessAsanaNewCommentEvent

--- app/comment_notifier/services.py
+++ app/comment_notifier/services.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from .models import AsanaComment
+from .models import AsanaComment, AsanaWebhookRequestData
 
 
 class ProcessAsanaNewCommentEvent:
@@ -18,15 +18,17 @@ class ProcessAsanaNewCommentEvent:
     def _event_to_comment_dto(self, events_data: dict) -> list[AsanaNewCommentEvent]:
         result = []
         for event in events_data["events"]:
-            event_dto = ProcessAsanaNewCommentEvent.AsanaNewCommentEvent(
-                comment_id=event["resource"]["gid"],
-                user_id=event["user"]["gid"],
-                task_id=event["parent"]["gid"],
-            )
-            result.append(event_dto)
+            if event["resource"]["resource_subtype"] == "comment_added":
+                event_dto = ProcessAsanaNewCommentEvent.AsanaNewCommentEvent(
+                    comment_id=event["resource"]["gid"],
+                    user_id=event["user"]["gid"],
+                    task_id=event["parent"]["gid"],
+                )
+                result.append(event_dto)
         return result
 
-    def process(self, events_data: dict) -> Result:
+    def process(self,  asana_webhook: AsanaWebhookRequestData) -> Result:
+        events_data = asana_webhook.payload
         asana_comments_dto = self._event_to_comment_dto(events_data=events_data)
         for comment_dto in asana_comments_dto:
             AsanaComment.objects.create(
@@ -34,6 +36,8 @@ class ProcessAsanaNewCommentEvent:
                 user_id=comment_dto.user_id,
                 task_id=comment_dto.task_id,
             )
+        asana_webhook.is_target_event = bool(asana_comments_dto)
+        asana_webhook.save()
         return ProcessAsanaNewCommentEvent.Result(
             created_comments_count=len(asana_comments_dto),
             comments=asana_comments_dto,
