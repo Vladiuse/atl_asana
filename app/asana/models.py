e5d69cd07a880b54dff0d2370a2faaa5936ce104
vladiuse <vladiuse@gmail.com>
2025-11-10 17:02:27 +0300

    add field messenger_code to user

--- app/asana/models.py
+++ app/asana/models.py
@@ -1,9 +1,9 @@
+from common.message_sender import Users
+from django.core.exceptions import ValidationError
 from django.db import models
 
-
-class Position(models.TextChoices):
-    BUYER = "buyer", "Баер"
-    FARMER = "farmer", "Фармер"
+from .constants import Position
+from .utils import get_asana_profile_url_by_id
 
 
 class AtlasUser(models.Model):
@@ -12,9 +12,27 @@ class AtlasUser(models.Model):
     email = models.EmailField(blank=True)
     name = models.CharField(max_length=254)
     avatar_url = models.URLField(blank=True)
+    messenger_code = models.CharField(
+        max_length=32,
+        choices=Users.choices(),
+        blank=True,
+    )
     position = models.CharField(
         blank=True,
         max_length=32,
         choices=Position.choices,
         verbose_name="Позиция",
     )
+
+    @property
+    def user_comment_mention(self) -> str:
+        return "@" + self.name
+
+    @property
+    def profile_url(self) -> str:
+        return get_asana_profile_url_by_id(profile_id=self.membership_id)
+
+    def clean(self) -> None:
+        super().clean()
+        if bool(self.messenger_code) != bool(self.position):
+            raise ValidationError("Оба поля messenger_code и position должны быть либо заполнены, либо пусты.")
