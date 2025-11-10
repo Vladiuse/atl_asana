974b8c49d5f28040f16648b3734fbf87aead1103
vladiuse <vladiuse@gmail.com>
2025-11-10 20:01:24 +0300

    update prettify_asana_comment_text_with_mentions

--- app/asana/services.py
+++ app/asana/services.py
@@ -1,7 +1,8 @@
 from .models import AtlasUser
 
 
-def prettify_asana_comment_text_with_mentions(text: str, asana_users: list[AtlasUser]) -> str:
+def prettify_asana_comment_text_with_mentions(text: str) -> str:
+    asana_users = AtlasUser.objects.all()
     for user in asana_users:
         text = text.replace(user.profile_url, user.user_comment_mention)
     return text
