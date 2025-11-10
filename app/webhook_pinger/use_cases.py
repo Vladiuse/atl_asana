8a722a6bac45047f35fef7c1947dfaf59448cda9
vladiuse <vladiuse@gmail.com>
2025-11-10 22:30:57 +0300

    add logging

--- app/webhook_pinger/use_cases.py
+++ app/webhook_pinger/use_cases.py
@@ -1,3 +1,5 @@
+import logging
+
 from asana.client import AsanaApiClient
 from asana.constants import ATLAS_WORKSPACE_ID
 from common import MessageSender
@@ -12,8 +14,9 @@ class PingWebhooksUseCase:
 
     def execute(self) -> dict:
         webhooks_db_count = Webhook.objects.count()
-        asana_webhooks_data = self.asana_api_client.get_webhooks(webhook_id=ATLAS_WORKSPACE_ID)
+        asana_webhooks_data = self.asana_api_client.get_webhooks(workspace_id=ATLAS_WORKSPACE_ID)
         atlas_webhook_ids = [asana_webhook["gid"] for asana_webhook in asana_webhooks_data]
+        logging.info("atlas_webhook_ids: %s", atlas_webhook_ids)
         if webhooks_db_count != len(atlas_webhook_ids):
             message = f"Число вебхуков не совпадает, на сервере {len(atlas_webhook_ids)}, в БД {webhooks_db_count}"
             self.message_sender.send_message(handler=MessageSender.KVA_USER, message=message)
