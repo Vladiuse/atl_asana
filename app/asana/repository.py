78d50c215106a261f88b8d3e364fac9f9b1b2302
vladiuse <vladiuse@gmail.com>
2025-11-10 14:46:33 +0300

    refactor repository

--- app/asana/repository.py
+++ app/asana/repository.py
@@ -1,10 +1,12 @@
 import logging
 
+from common.exception import AppException
+
 from asana.client import AsanaApiClient
 
 from .constants import ATLAS_WORKSPACE_ID
 from .models import AtlasUser
-from common.exception import AppException
+
 
 class AsanaUserRepository:
     def __init__(self, api_client: AsanaApiClient):
@@ -24,36 +26,12 @@ class AsanaUserRepository:
             email=email,
         )
 
-    def get_user(self, membership_id: int) -> AtlasUser:
-        try:
-            user = AtlasUser.objects.get(membership_id=membership_id)
-            logging.info("Get user from DB")
-            return user
-        except AtlasUser.DoesNotExist:
-            logging.info("Try load user from asana")
-            return self.create_by_membership_id(membership_id=membership_id)
-
-    def update_all(self) -> None:
-        atlas_asana_memberships = self.api_client.get_workspace_memberships_for_workspace(workspace_id=ATLAS_WORKSPACE_ID)
-        logging.info("Memberships in asana: %s", len(atlas_asana_memberships))
-        exist_memberships_in_db = [str(i) for i in AtlasUser.objects.values_list("membership_id", flat=True)]
-        logging.info("Memberships in DB: %s", len(exist_memberships_in_db))
-        new_created = 0
-        for membership_data in atlas_asana_memberships:
-            membership_id = membership_data["gid"]
-            if membership_data["gid"] not in exist_memberships_in_db:
-                logging.info("Detect new Memberships: %s", membership_id)
-                user_data = self.api_client.get_user(user_id=membership_data["user"]["gid"])
-                self._create_user_by_data(membership_data=membership_data, user_data=user_data)
-                new_created+= 1
-        logging.info("New created: %s", new_created)
-
-    def create_by_membership_id(self, membership_id: int) -> AtlasUser:
+    def _create_by_membership_id(self, membership_id: int) -> AtlasUser:
         membership_data = self.api_client.get_workspace_membership(membership_id=membership_id)
         user_data = self.api_client.get_user(user_id=membership_data["user"]["gid"])
         return self._create_user_by_data(membership_data=membership_data, user_data=user_data)
 
-    def create_by_user_id(self, user_id: int) -> AtlasUser:
+    def _create_by_user_id(self, user_id: int) -> AtlasUser:
         user_data = self.api_client.get_user(user_id=user_id)
         atlas_user_membership_id = None
         user_memberships = self.api_client.get_workspace_memberships_for_user(user_id=user_id)
@@ -67,8 +45,28 @@ class AsanaUserRepository:
         membership_data = self.api_client.get_workspace_membership(membership_id=atlas_user_membership_id)
         return self._create_user_by_data(membership_data=membership_data, user_data=user_data)
 
+    def get(self, membership_id: int) -> AtlasUser:
+        try:
+            user = AtlasUser.objects.get(membership_id=membership_id)
+            logging.info("Get user from DB")
+            return user
+        except AtlasUser.DoesNotExist:
+            logging.info("Try load user from asana")
+            return self._create_by_membership_id(membership_id=membership_id)
 
-
-
-
-
+    def update_all(self) -> None:
+        atlas_asana_memberships = self.api_client.get_workspace_memberships_for_workspace(
+            workspace_id=ATLAS_WORKSPACE_ID
+        )
+        logging.info("Memberships in asana: %s", len(atlas_asana_memberships))
+        exist_memberships_in_db = [str(i) for i in AtlasUser.objects.values_list("membership_id", flat=True)]
+        logging.info("Memberships in DB: %s", len(exist_memberships_in_db))
+        new_created = 0
+        for membership_data in atlas_asana_memberships:
+            membership_id = membership_data["gid"]
+            if membership_data["gid"] not in exist_memberships_in_db:
+                logging.info("Detect new Memberships: %s", membership_id)
+                user_data = self.api_client.get_user(user_id=membership_data["user"]["gid"])
+                self._create_user_by_data(membership_data=membership_data, user_data=user_data)
+                new_created += 1
+        logging.info("New created: %s", new_created)
