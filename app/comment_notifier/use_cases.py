6ea32bf760172aca906111b60e57772efce82543
vladiuse <vladiuse@gmail.com>
2025-11-11 20:24:48 +0300

    add FetchCommentTaskUrls

--- app/comment_notifier/use_cases.py
+++ app/comment_notifier/use_cases.py
@@ -1,6 +1,7 @@
 import logging
 
 from asana.client import AsanaApiClient
+from asana.client.exception import AsanaForbiddenError, AsanaNotFoundError
 from asana.constants import SOURCE_DIV_PROBLEMS_REQUESTS_COMPLETE_SECTION_ID, SOURCE_DIV_PROBLEMS_REQUESTS_PROJECT_ID
 
 from .models import AsanaComment
@@ -30,7 +31,7 @@ class FetchMissingProjectCommentsUseCase:
                 result.append(section_data)
         return result
 
-    def execute(self) -> None:
+    def execute(self) -> dict:
         new_comments_count = 0
         exists_comment_ids = set(AsanaComment.objects.values_list("comment_id", flat=True))
         logging.info("exists_comment_ids: %s", len(exists_comment_ids))
@@ -55,4 +56,36 @@ class FetchMissingProjectCommentsUseCase:
                                 task_id=task_id,
                             )
                             new_comments_count += 1
-        print(new_comments_count)
+        logging.info("new_comments_count: %s", new_comments_count)
+        return {"new_comments_count": new_comments_count}
+
+
+class FetchCommentTaskUrls:
+    def __init__(self, asana_api_client: AsanaApiClient):
+        self.asana_api_client = asana_api_client
+
+    def execute(self) -> dict:
+        tasks_ids_without_task_urls = (
+            AsanaComment.objects.filter(task_url="", is_deleted=False).values_list("task_id", flat=True).distinct()
+        )
+        logging.info("comments_without_task_urls: %s", len(tasks_ids_without_task_urls))
+        updated_tasks = 0
+        deleted_tasks = 0
+        for task_id in tasks_ids_without_task_urls:
+            logging.info("Comment id: %s", task_id)
+            try:
+                task_data = self.asana_api_client.get_task(task_id=task_id)
+                task_url = task_data["permalink_url"]
+                AsanaComment.objects.filter(task_id=task_id).update(task_url=task_url)
+                updated_tasks += 1
+            except (AsanaForbiddenError, AsanaNotFoundError) as error:
+                logging.info("Cant get task: %s", error)
+                comment_models = AsanaComment.objects.filter(task_id=task_id)
+                for comment_model in comment_models:
+                    comment_model.mark_as_deleted()
+                deleted_tasks += 1
+        return {
+            "updated_comments": updated_tasks,
+            "deleted_tasks": deleted_tasks,
+            "to_load": len(tasks_ids_without_task_urls),
+        }
