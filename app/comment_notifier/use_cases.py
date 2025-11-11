b47e2604bea963c65248f8d0fc318effde005134
vladiuse <vladiuse@gmail.com>
2025-11-11 18:05:48 +0300

    add FetchMissingProjectCommentsUseCase usecase

--- app/comment_notifier/use_cases.py
+++ app/comment_notifier/use_cases.py
@@ -1,4 +1,5 @@
 from asana.client import AsanaApiClient
+from asana.constants import SOURCE_DIV_PROBLEMS_REQUESTS_COMPLETE_SECTION_ID, SOURCE_DIV_PROBLEMS_REQUESTS_PROJECT_ID
 
 
 class AsanaCommentNotifierUseCase:
@@ -7,3 +8,22 @@ class AsanaCommentNotifierUseCase:
 
     def execute(self) -> None:
         pass
+
+
+class FetchMissingProjectCommentsUseCase:
+    DIV_PROJECT_IGNORED_SECTIONS = [SOURCE_DIV_PROBLEMS_REQUESTS_COMPLETE_SECTION_ID]
+
+    def __init__(self, asana_api_client: AsanaApiClient):
+        self.asana_api_client = asana_api_client
+
+    def _project_active_section_ids(self) -> list[int]:
+        result = []
+        sections = self.asana_api_client.get_project_sections(project_id=SOURCE_DIV_PROBLEMS_REQUESTS_PROJECT_ID)
+        for section_data in sections:
+            if section_data["gid"] not in self.DIV_PROJECT_IGNORED_SECTIONS:
+                result.append(section_data["gid"])
+        return result
+
+    def execute(self) -> None:
+        sections = self._project_active_section_ids()
+        print(sections)
