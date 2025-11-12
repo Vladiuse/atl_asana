181f34e853dabc144f6c72ad5b1c091a3e71e534
vladiuse <vladiuse@gmail.com>
2025-11-12 21:32:02 +0300

    refactor tasks

--- app/webhook_pinger/tasks.py
+++ app/webhook_pinger/tasks.py
@@ -9,17 +9,22 @@ message_sender = MessageSender(request_sender=RequestsSender())
 asana_client = AsanaApiClient(api_key=settings.ASANA_API_KEY)
 
 
-@shared_task
-def ping_asana_webhook() -> dict:
-    use_case = PingWebhooksUseCase(
-        asana_api_client=asana_client,
-        message_sender=MessageSender(request_sender=RequestsSender()),
-    )
+@shared_task(bind=True, max_retries=2, default_retry_delay=30)
+def ping_asana_webhook(self) -> dict | None:
     try:
-        return use_case.execute()
-    except Exception as error:
-        message_sender.send_message(
-            handler="kva_test",
-            message=f"ping_asana_webhook {error.__class__.__name__}: {error}",
+        use_case = PingWebhooksUseCase(
+            asana_api_client=asana_client,
+            message_sender=MessageSender(request_sender=RequestsSender()),
         )
-        raise error
+        return use_case.execute()
+    except Exception as error:  # noqa: BLE001
+        self.retry(exc=error)
+
+
+
+@shared_task(bind=True, max_retries=2, default_retry_delay=5)
+def test_task(self) -> None:
+    try:
+        raise ZeroDivisionError
+    except Exception as error:  # noqa: BLE001
+        self.retry(exc=error)
