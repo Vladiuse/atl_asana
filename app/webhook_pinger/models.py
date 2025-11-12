9293b9931137d1db4715acee662dda71c2746a63
vladiuse <vladiuse@gmail.com>
2025-11-12 15:54:16 +0300

    add fields to model

--- app/webhook_pinger/models.py
+++ app/webhook_pinger/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 
 class Webhook(models.Model):
-    webhook_id = models.IntegerField(unique=True)
+    webhook_id = models.CharField(unique=True)
+    resource_name = models.CharField(max_length=100, blank=True)
+    resource_type = models.CharField(max_length=100, blank=True)
+    target = models.URLField(blank=True)
     description = models.CharField(max_length=254, blank=True)
