bd4b57fa42756b43bb82880aa6374f5fff27ed1f
vladiuse <vladiuse@gmail.com>
2025-11-12 21:31:57 +0300

    add celery signals

--- app/atl_asana/celery.py
+++ app/atl_asana/celery.py
@@ -7,3 +7,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atl_asana.settings')
 app = Celery("atl_asana")
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+from .celery_signals import notify_in_telegram  # noqa: E402, F401
---
+++ app/atl_asana/celery_signals.py
@@ -0,0 +1,19 @@
+from celery.signals import task_failure
+from common import MessageSender, RequestsSender
+
+message_sender = MessageSender(request_sender=RequestsSender())
+
+
+@task_failure.connect
+def notify_in_telegram(  # noqa: PLR0913
+    sender=None,
+    task_id=None,  # noqa: ARG001
+    exception=None,
+    args=None,  # noqa: ARG001
+    kwargs=None,  # noqa: ARG001
+    traceback=None,  # noqa: ARG001
+    einfo=None,
+    **kw,  # noqa: ARG001
+) -> None:
+    message = f"Task failed!\nTask id: {task_id}\nTask name: {sender.name}\nException: {exception}\nInfo: {einfo}\n"
+    message_sender.send_message(handler=MessageSender.KVA_USER, message=message)
