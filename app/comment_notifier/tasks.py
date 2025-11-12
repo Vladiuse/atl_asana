8130c6cb09e992b41d0e893128f48ee92a22db16
vladiuse <vladiuse@gmail.com>
2025-11-12 21:36:40 +0300

    refactor tasks

--- app/comment_notifier/tasks.py
+++ app/comment_notifier/tasks.py
@@ -13,54 +13,32 @@ asana_api_client = AsanaApiClient(api_key=settings.ASANA_API_KEY)
 message_sender = MessageSender(request_sender=RequestsSender())
 
 
-@shared_task
-def process_asana_new_comments_task(asana_webhook_id: int) -> dict:
+@shared_task(bind=True, max_retries=2, default_retry_delay=3)
+def process_asana_new_comments_task(self, asana_webhook_id: int) -> dict | None:
     try:
         asana_webhook = AsanaWebhookRequestData.objects.get(pk=asana_webhook_id)
         result = ProcessAsanaNewCommentEvent().process(asana_webhook)
         return asdict(result)
-    except Exception as error:
-        message_sender.send_message(
-            handler="kva_test",
-            message=f"process_asana_new_comments {error.__class__.__name__}: {error}",
-        )
-        raise error
+    except Exception as error:  # noqa: BLE001
+        self.retry(exc=error)
 
 
-@shared_task
-def fetch_comment_tasks_urls_task() -> dict:
-    try:
-        use_case = FetchCommentTaskUrls(asana_api_client=asana_api_client)
-        return use_case.execute()
-    except Exception as error:
-        message_sender.send_message(
-            handler="kva_test",
-            message=f"fetch_comment_tasks_urls_task {error.__class__.__name__}: {error}",
-        )
-        raise error
-
-
-@shared_task
-def fetch_missing_project_comments_task() -> dict:
+@shared_task(bind=True, max_retries=2, default_retry_delay=60)
+def fetch_missing_project_comments_task(self) -> dict | None:
     try:
         use_case = FetchMissingProjectCommentsUseCase(asana_api_client=asana_api_client)
         return use_case.execute()
-    except Exception as error:
-        message_sender.send_message(
-            handler="kva_test",
-            message=f"fetch_missing_project_comments_task {error.__class__.__name__}: {error}",
-        )
-        raise error
+    except Exception as error:  # noqa: BLE001
+        self.retry(exc=error)
 
 
-@shared_task
+@shared_task()
 def notify_new_asana_comments_tasks() -> dict:
-    try:
-        use_case = AsanaCommentNotifierUseCase(asana_api_client=asana_api_client, message_sender=message_sender)
-        return use_case.execute()
-    except Exception as error:
-        message_sender.send_message(
-            handler="kva_test",
-            message=f"notify_new_asana_comments_tasks {error.__class__.__name__}: {error}",
-        )
-        raise error
+    use_case = AsanaCommentNotifierUseCase(asana_api_client=asana_api_client, message_sender=message_sender)
+    return use_case.execute()
+
+
+@shared_task()
+def fetch_comment_tasks_urls_task() -> dict:
+    use_case = FetchCommentTaskUrls(asana_api_client=asana_api_client)
+    return use_case.execute()
