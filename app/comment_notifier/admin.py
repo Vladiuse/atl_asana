9b8b3f76d80842088da9feb09b7ca25513cd1db6
vladiuse <vladiuse@gmail.com>
2025-11-14 18:21:15 +0300

    add some actions in admin

--- app/comment_notifier/admin.py
+++ app/comment_notifier/admin.py
@@ -10,10 +10,12 @@ from django.db.models import QuerySet
 from django.http import HttpRequest
 from django.utils.html import format_html
 from django.utils.text import Truncator
+from django import forms
 
 from comment_notifier.services import AsanaCommentNotifier
 
 from .models import AsanaComment, AsanaWebhookProject, AsanaWebhookRequestData
+from .services import LoadAdditionalInfoForWebhookProject
 from .tasks import fetch_comment_tasks_urls_task, fetch_missing_project_comments_task
 from .use_cases import FetchCommentsAdditionalInfoUseCase
 
@@ -26,8 +28,47 @@ repository = AsanaUserRepository(api_client=asana_client)
 
 @admin.register(AsanaWebhookProject)
 class AsanaProjectAdmin(admin.ModelAdmin):
-    list_display = ["name", "secret"]
+    list_display = ["name", "secret", "project_name", "project_url_short"]
     list_display_links = ["name"]
+    actions = ["update_project_data"]
+
+    def save_model(
+        self,
+        request: HttpRequest,
+        obj: AsanaWebhookProject,
+        form: forms.ModelForm,
+        change: bool,
+    ) -> None:
+        super().save_model(request, obj, form, change)
+        service = LoadAdditionalInfoForWebhookProject(asana_api_client=asana_client)
+        try:
+            service.load(webhook_project=obj)
+        except AsanaApiClientError:
+            self.message_user(
+                request,
+                message=f"Не удаллось обновить проект: {obj}",
+                level=messages.ERROR,
+            )
+
+    @admin.display(description="Task URL")
+    def project_url_short(self, obj: AsanaWebhookProject) -> str:
+        if obj.project_url:
+            return format_html('<a href="{}" target="_blank">{}</a>', obj.project_url, "Открыть")
+        return "—"
+
+    @admin.action(description="Обновить данные по проекту")
+    def update_project_data(self, request: HttpRequest, queryset: QuerySet) -> None:
+        service = LoadAdditionalInfoForWebhookProject(asana_api_client=asana_client)
+        for webhook_project in queryset:
+            try:
+                service.load(webhook_project=webhook_project)
+            except AsanaApiClientError:
+                self.message_user(
+                    request,
+                    message=f"Не удаллось обновить проект: {webhook_project}",
+                    level=messages.ERROR,
+                )
+        self.message_user(request, message="Данные обновлены")
 
 
 @admin.register(AsanaWebhookRequestData)
