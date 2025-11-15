1551c301ce9fabab9e26d3bafb396dd19f2b0555
vladiuse <vladiuse@gmail.com>
2025-11-15 16:38:07 +0300

    fix message tasks

--- app/message_sender/tasks.py
+++ app/message_sender/tasks.py
@@ -3,9 +3,9 @@ from common import MessageSender, RequestsSender
 from common.message_sender import UserTag
 
 message_sender = MessageSender(request_sender=RequestsSender())
+DELAY_RETRY = 60
 
-
-@shared_task(bind=True, max_retries=2, default_60=60)
+@shared_task(bind=True, max_retries=2, default_retry_delay=DELAY_RETRY)
 def send_log_message_task(self, message: str) -> dict | None:
     try:
         message_sender.send_log_message(message=message)
@@ -13,7 +13,7 @@ def send_log_message_task(self, message: str) -> dict | None:
         self.retry(exc=error)
 
 
-@shared_task(bind=True, max_retries=2, default_60=60)
+@shared_task(bind=True, max_retries=2, default_retry_delay=DELAY_RETRY)
 def send_message_to_user_task(self, user_tags: list[str], message: str) -> dict | None:
     try:
         user_tags = [UserTag(value) for value in user_tags]
@@ -22,7 +22,7 @@ def send_message_to_user_task(self, user_tags: list[str], message: str) -> dict 
         self.retry(exc=error)
 
 
-@shared_task(bind=True, max_retries=2, default_60=60)
+@shared_task(bind=True, max_retries=2, default_retry_delay=DELAY_RETRY)
 def send_message_task(self, handler: str, message: str) -> dict | None:
     try:
         message_sender.send_message(handler=handler, message=message)
