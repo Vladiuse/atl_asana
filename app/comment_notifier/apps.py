bbaf9d379872a248c3a898f98202eeb4ea0b621e
vladiuse <vladiuse@gmail.com>
2025-11-17 21:15:02 +0300

    remove sync from app

--- app/comment_notifier/apps.py
+++ app/comment_notifier/apps.py
@@ -5,7 +5,3 @@ class CommentNotifierConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "comment_notifier"
 
-    def ready(self) -> None:
-        from .services import SenderRegistrySynchronizer
-
-        SenderRegistrySynchronizer().synchronize()
