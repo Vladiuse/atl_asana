f25020a864b00995e7641145a2142ca57037124f
vladiuse <vladiuse@gmail.com>
2025-11-17 13:13:29 +0300

    add few comments

--- app/comment_notifier/services.py
+++ app/comment_notifier/services.py
@@ -201,6 +201,10 @@ class LoadAdditionalInfoForProjectIgnoredSection:
     asana_api_client: AsanaApiClient
 
     def load(self, project_ignored_section: ProjectIgnoredSection) -> None:
+        """
+        Raises:
+             AsanaApiClientError: if cant get some data from asana
+        """
         section_data = self.asana_api_client.get_section(project_ignored_section.section_id)
         project_ignored_section.section_name = section_data["name"]
         project_ignored_section.save()
@@ -211,6 +215,10 @@ class LoadCommentsAdditionalInfo:
     asana_api_client: AsanaApiClient
 
     def load(self, queryset: QuerySet[AsanaComment]) -> dict:
+        """
+        Raises:
+             AsanaApiClientError: if cant get some data from asana
+        """
         comments_to_update = queryset.filter(
             Q(text="") | Q(task_url=""),
             is_deleted=False,
