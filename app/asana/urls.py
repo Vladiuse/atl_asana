38e4f5321e0b6b3e92f6ad511bca0d8d89d04c51
vladiuse <vladiuse@gmail.com>
2025-12-09 19:15:33 +0300

    add urls

--- app/asana/urls.py
+++ app/asana/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "asana"
+
 urlpatterns = [
-    path("webhook/<str:webhook_name_name>/", views.AsanaWebhookView.as_view()),
+    path("webhook/<str:webhook_name>/", views.AsanaWebhookView.as_view(), name="webhook"),
 ]
