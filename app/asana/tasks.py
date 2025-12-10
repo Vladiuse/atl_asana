30c1252032c56f1470e74e8a9339e5ace430bfff
vladiuse <vladiuse@gmail.com>
2025-12-10 18:11:43 +0300

    fix bug

--- app/asana/tasks.py
+++ app/asana/tasks.py
@@ -10,7 +10,7 @@ from .models import AsanaWebhookRequestData
 @shared_task(bind=True, max_retries=1, default_retry_delay=60 * 3)
 def process_asana_webhook_task(self, asana_webhook_data_id: int) -> dict | None:
     try:
-        webhook_data = AsanaWebhookRequestData(pk=asana_webhook_data_id)
+        webhook_data = AsanaWebhookRequestData.objects.get(pk=asana_webhook_data_id)
         dispatcher = WebhookDispatcher()
         dispatcher_result = dispatcher.dispatch(webhook_data=webhook_data)
         return asdict(dispatcher_result)
