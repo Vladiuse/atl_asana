1b5313f645d519cb44603081e59b922b459c137a
vladiuse <vladiuse@gmail.com>
2025-12-10 16:11:43 +0300

    add admin

--- app/creative_quality/admin.py
+++ app/creative_quality/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+from .models import Task, Creative
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("task_id", "task_name", "status", "is_complete", "assignee_id", "bayer_code", "created")
+    list_filter = ("status", "is_complete",)
+    search_fields = ("task_id", "task_name", "assignee_id", "bayer_code")
+    ordering = ("-created",)
+
+
+@admin.register(Creative)
+class CreativeAdmin(admin.ModelAdmin):
+    list_display = ("task", "is_rated", "hook", "hold", "crt", "need_rated_at", "created")
+    list_filter = ("is_rated", "need_rated_at",)
+    search_fields = ("task__task_id", "task__task_name")
+    ordering = ("-created",)
