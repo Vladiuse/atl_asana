3bdd45957433b61a0ca8fe005ec4ef65b60aa5f1
vladiuse <vladiuse@gmail.com>
2025-12-11 18:44:10 +0300

    add soem code

--- app/creative_quality/urls.py
+++ app/creative_quality/urls.py
@@ -6,5 +6,10 @@ app_name = "creative_quality"
 
 
 urlpatterns = [
-    path("estimate-creative/<int:creative_id>/<str:task_id>/", views.CreativeUpdateView.as_view()),
+    path(
+        "estimate-creative/<int:creative_id>/<str:task_id>/update/",
+        views.CreativeUpdateView.as_view(),
+        name="update-creative",
+    ),
+    path("estimate-creative/<int:creative_id>/<str:task_id>/", views.CreativeDetailView.as_view()),
 ]
--- app/creative_quality/views.py
+++ app/creative_quality/views.py
@@ -13,16 +13,37 @@ class CreativeUpdateView(View):
     def get(self, request: HttpRequest, creative_id: int, task_id: str) -> HttpResponse:
         creative = get_object_or_404(Creative, pk=creative_id, task__task_id=task_id)
         form = CreativeForm(instance=creative)
-        return render(request, self.template_name, {"form": form, "creative": creative})
+        return render(
+            request,
+            self.template_name,
+            {
+                "form": form,
+                "creative": creative,
+            },
+        )
 
     def post(self, request: HttpRequest, creative_id: int, task_id: str) -> HttpResponse:
-        creative = get_object_or_404(Creative, pk=creative_id, task__id=task_id)
+        creative = get_object_or_404(Creative, pk=creative_id, task__task_id=task_id)
         form = CreativeForm(request.POST, instance=creative)
 
         if form.is_valid():
             creative_obj = form.save(commit=False)
             creative_obj.status = CreativeStatus.RATED
             creative_obj.save()
-            return redirect("/", pk=creative.pk)
+            return redirect("creative_quality:creative_detail", creative_id=creative.pk, task_id=creative.task.task_id)
 
         return render(request, self.template_name, {"form": form, "creative": creative})
+
+class CreativeDetailView(View):
+    template_name = "creative_quality/creative/detail.html"
+
+    def get(self, request: HttpRequest, creative_id: int, task_id: str) -> HttpResponse:
+        creative = get_object_or_404(Creative, pk=creative_id, task__task_id=task_id)
+        return render(
+            request,
+            self.template_name,
+            {
+                "creative": creative,
+                "creative_statuses": CreativeStatus,
+            },
+        )
