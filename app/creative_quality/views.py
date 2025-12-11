e0d57cb2f520ace2d1e6a76b92c1bdad7abe4a60
vladiuse <vladiuse@gmail.com>
2025-12-11 16:25:58 +0300

    add view

---
+++ app/creative_quality/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views import View
+
+from creative_quality.models import Creative, CreativeStatus
+
+from .forms import CreativeForm
+
+
+class CreativeUpdateView(View):
+    template_name = "creative_quality/creative/update.html"
+
+    def get(self, request: HttpRequest, creative_id: int, task_id: str) -> HttpResponse:
+        creative = get_object_or_404(Creative, pk=creative_id, task__id=task_id)
+        form = CreativeForm(instance=creative)
+        return render(request, self.template_name, {"form": form, "creative": creative})
+
+    def post(self, request: HttpRequest, creative_id: int, task_id: str) -> HttpResponse:
+        creative = get_object_or_404(Creative, pk=creative_id, task__id=task_id)
+        form = CreativeForm(request.POST, instance=creative)
+
+        if form.is_valid():
+            creative_obj = form.save(commit=False)
+            creative_obj.status = CreativeStatus.RATED
+            creative_obj.save()
+            return redirect("/", pk=creative.pk)
+
+        return render(request, self.template_name, {"form": form, "creative": creative})
