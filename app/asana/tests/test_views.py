1e75a5aad6fea93f6a2b2e1a0180b2488bddbdcc
vladiuse <vladiuse@gmail.com>
2025-12-12 21:52:19 +0300

    add pytest in test

--- app/asana/tests/test_views.py
+++ app/asana/tests/test_views.py
@@ -1,55 +1,60 @@
 from http import HTTPStatus
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, Mock
 
-from django.test import Client, TestCase
+import pytest
+from django.test import Client
 from django.urls import reverse
 
-from asana.models import AsanaWebhook, AsanaWebhookRequestData
 from asana.constants import AsanaResourceType
+from asana.models import AsanaWebhook, AsanaWebhookRequestData
 
 
-@patch("asana.views.process_asana_webhook_task.delay", return_value=None)
-class WebhookViewTest(TestCase):
-    def setUp(self):
-        self.factory = Client()
+@pytest.fixture()
+def mock_task_delay(monkeypatch: pytest.MonkeyPatch) -> MagicMock:
+    mock = Mock()
+    monkeypatch.setattr("asana.views.process_asana_webhook_task.delay", mock)
+    return mock
 
-    def test_get_method(self, mock_task: MagicMock):
-        _ = mock_task
+
+@pytest.mark.django_db()
+class TestWebhookView:
+    def test_get_method(self, mock_task_delay: MagicMock, client: Client):
+        _ = mock_task_delay
         url = reverse("asana:webhook", kwargs={"webhook_name": "x"})
-        response = self.factory.get(url)
+        response = client.get(url)
         assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
 
-    def test_no_webhook_in_db(self, mock_task: MagicMock):
-        _ = mock_task
+    def test_no_webhook_in_db(self, mock_task_delay: MagicMock, client: Client):
+        _ = mock_task_delay
         url = reverse("asana:webhook", kwargs={"webhook_name": "x"})
-        response = self.factory.post(url)
+        response = client.post(url)
         assert response.status_code == HTTPStatus.NOT_FOUND, f"actual: {response.status_code}"
 
-    def test_no_yet_secret_key(self, mock_task: MagicMock):
-        _ = mock_task
+    def test_no_yet_secret_key(self, mock_task_delay: MagicMock, client: Client):
+        _ = mock_task_delay
         webhook_name = "xxxyyyxxx"
         AsanaWebhook.objects.create(name=webhook_name, resource_id="123", resource_type=AsanaResourceType.PROJECT)
         url = reverse("asana:webhook", kwargs={"webhook_name": webhook_name})
-        response = self.factory.post(url)
+        response = client.post(url)
         assert response.status_code == HTTPStatus.BAD_REQUEST, f"actual: {response.status_code}"
         response_data = response.json()
         assert response_data["success"] is False
         assert webhook_name in response_data["message"]
 
-    def test_send_secret_key(self, mock_task: MagicMock):
-        _ = mock_task
+    def test_send_secret_key(self, mock_task_delay: MagicMock, client: Client):
+        _ = mock_task_delay
         webhook = AsanaWebhook.objects.create(name="name", resource_id="123", resource_type=AsanaResourceType.PROJECT)
         headers = {
             "X-Hook-Secret": "xxx",
         }
         url = reverse("asana:webhook", kwargs={"webhook_name": "name"})
-        response = self.factory.post(url, headers=headers)
+        response = client.post(url, headers=headers)
         assert response.status_code == HTTPStatus.CREATED
         webhook.refresh_from_db()
         assert webhook.secret == "xxx"
 
-    def test_return_secret_key(self, mock_task: MagicMock):
-        _ = mock_task
+    def test_return_secret_key(self, mock_task_delay: MagicMock, client: Client):
+        _ = mock_task_delay
         AsanaWebhook.objects.create(
             name="name",
             resource_id="123",
@@ -57,12 +62,12 @@ class WebhookViewTest(TestCase):
             secret="xxx",
         )
         url = reverse("asana:webhook", kwargs={"webhook_name": "name"})
-        response = self.factory.post(url)
+        response = client.post(url)
         assert response.status_code == HTTPStatus.OK
         assert response.headers["X-Hook-Secret"] == "xxx"
 
-    def test_save_webhook_data(self, mock_task: MagicMock):
-        _ = mock_task
+    def test_save_webhook_data(self, mock_task_delay: MagicMock, client: Client):
+        _ = mock_task_delay
         webhook = AsanaWebhook.objects.create(
             name="name",
             resource_id="123",
@@ -71,7 +76,7 @@ class WebhookViewTest(TestCase):
         )
         url = reverse("asana:webhook", kwargs={"webhook_name": "name"})
         data = {"y": "y"}
-        response = self.factory.post(
+        response = client.post(
             url,
             data=data,
             content_type="application/json",
@@ -82,7 +87,7 @@ class WebhookViewTest(TestCase):
         assert webhook_data.payload == data, f"actual: {webhook_data.payload}"
         assert webhook_data.webhook == webhook
 
-    def test_run_task(self, mock_task: MagicMock):
+    def test_run_task(self, mock_task_delay: MagicMock, client: Client):
         AsanaWebhook.objects.create(
             name="name",
             resource_id="123",
@@ -91,7 +96,7 @@ class WebhookViewTest(TestCase):
         )
         url = reverse("asana:webhook", kwargs={"webhook_name": "name"})
         data = {"y": "y"}
-        response = self.factory.post(
+        response = client.post(
             url,
             data=data,
             content_type="application/json",
@@ -99,4 +104,4 @@ class WebhookViewTest(TestCase):
         assert response.status_code == HTTPStatus.OK
         assert AsanaWebhookRequestData.objects.count() == 1
         webhook_data = AsanaWebhookRequestData.objects.last()
-        mock_task.assert_called_once_with(asana_webhook_data_id=webhook_data.pk)
+        mock_task_delay.assert_called_once_with(asana_webhook_data_id=webhook_data.pk)
