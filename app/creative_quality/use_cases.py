4f0f0c103c112e409540367cdfed0a9195bd3ad2
vladiuse <vladiuse@gmail.com>
2025-12-12 16:00:17 +0300

    add usecase

---
+++ app/creative_quality/use_cases.py
@@ -0,0 +1,16 @@
+from creative_quality.models import Task
+from creative_quality.services import CreativeService
+
+
+class CreateCreativesForNewTasksUseCase:
+    def __init__(self, creative_service: CreativeService):
+        self.creative_service = creative_service
+
+    def execute(self) -> dict:
+        new_tasks = Task.objects.needs_update()
+        created_count = 0
+        for task in new_tasks:
+            creative = self.creative_service.create_creative(creative_task=task)
+            if creative is not None:
+                created_count += 1
+        return {"created_count": created_count}
