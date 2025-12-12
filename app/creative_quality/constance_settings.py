1bb48b3d176a873f37dca7b99ab9dce619e6c57e
vladiuse <vladiuse@gmail.com>
2025-12-12 15:20:49 +0300

    add few constance

--- app/creative_quality/constance_settings.py
+++ app/creative_quality/constance_settings.py
@@ -4,8 +4,27 @@ CONSTANCE_CONFIG = {
         "Через сколько дней присылать сообщение об оценке креатива",
         int,
     ),
+    "NEXT_SUCCESS_REMINDER_DELTA": (
+        24,
+        "Через сколько часов отправлять повторное смс об оценке креатива",
+        int,
+    ),
+    "FAILURE_RETRY_INTERVAL": (
+        2,
+        "Через сколько часов попытаться отправит смс об оценке при ошибке отправки",
+        int,
+    ),
+    "SEND_REMINDER_TRY_COUNT": (
+        3,
+        "Число попыток для отправки сообщений, успешных и нет",
+        int,
+    ),
 }
 
 CONSTANCE_CONFIG_FIELDSETS = {
-    "CREATIVE_ESTIMATE": ("NEED_RATED_AT",),
+    "CREATIVE_ESTIMATE": (
+        "NEED_RATED_AT",
+        "NEXT_SUCCESS_REMINDER_DELTA",
+        "FAILURE_RETRY_INTERVAL",
+    ),
 }
