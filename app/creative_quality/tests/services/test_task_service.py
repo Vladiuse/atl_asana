9335f8d8331448e6555c9ef5ebb2666871eb86ca
vladiuse <vladiuse@gmail.com>
2025-12-13 18:59:51 +0300

    add few test

--- app/creative_quality/tests/services/test_task_service.py
+++ app/creative_quality/tests/services/test_task_service.py
@@ -2,8 +2,10 @@ from unittest.mock import Mock
 
 import pytest
 from asana.client import AsanaApiClient
+from asana.client.exception import AsanaApiClientError, AsanaForbiddenError, AsanaNotFoundError
 from constance.test import override_config
 
+from creative_quality.models import Task
 from creative_quality.services import TaskService
 
 GET_DTO_TEST_DATA = [
@@ -75,3 +77,26 @@ class TestTaskService:
                 service._get_task_dto(task_data=data)
         else:
             assert service._get_task_dto(task_data=data) == expected
+
+    def test_mark_completed_no_error(self, mock_asana_client: Mock):
+        task = Mock(spec=Task)
+        service = TaskService(asana_api_client=mock_asana_client)
+        service.mark_completed(task=task)
+        assert mock_asana_client.mark_task_completed.called
+        assert task.is_completed is True
+        assert task.save.called
+
+    @pytest.mark.parametrize("error_type", [AsanaNotFoundError, AsanaForbiddenError])
+    def test_mark_completed_deleted_task(self, mock_asana_client: Mock, error_type: type[AsanaApiClientError]):
+        task = Mock(spec=Task)
+        mock_asana_client.mark_task_completed.side_effect = error_type("error")
+        service = TaskService(asana_api_client=mock_asana_client)
+        service.mark_completed(task=task)
+        assert task.mark_deleted.called
+
+    def test_unexpected_asana_error(self, mock_asana_client: Mock):
+        task = Mock(spec=Task)
+        mock_asana_client.mark_task_completed.side_effect = AsanaApiClientError("boom")
+        service = TaskService(asana_api_client=mock_asana_client)
+        with pytest.raises(AsanaApiClientError):
+            service.mark_completed(task=task)
