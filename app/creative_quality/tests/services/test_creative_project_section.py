d4440b0fd5a2453f0293e8ceb46970e55fe08512
vladiuse <vladiuse@gmail.com>
2025-12-13 17:46:23 +0300

    add few tests

---
+++ app/creative_quality/tests/services/test_creative_project_section.py
@@ -0,0 +1,25 @@
+from unittest.mock import Mock
+
+import pytest
+from asana.client import AsanaApiClient
+
+from creative_quality.models import CreativeProjectSection
+from creative_quality.services import CreativeProjectSectionService
+
+
+@pytest.mark.django_db()
+class TestCreativeProjectSectionService:
+
+    def test_correct_save(self):
+        mock_sana = Mock(spec=AsanaApiClient)
+        mock_sana.get_section.return_value = {
+            "name": "111",
+            "project": {"name": "222"},
+        }
+        service = CreativeProjectSectionService(asana_api_client=mock_sana)
+        section = CreativeProjectSection.objects.create(section_id=100)
+        service.update_additional_info(creative_project_section=section)
+        section.refresh_from_db()
+        assert section.section_name == "111"
+        assert section.project_name == "222"
+        mock_sana.get_section.assert_called_once_with(section_id=100)
