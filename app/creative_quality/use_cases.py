819917b070da544de9855fdf920075907feb278c
vladiuse <vladiuse@gmail.com>
2025-12-15 19:52:53 +0300

    add SendCreativesToGoogleSheetUseCase

--- app/creative_quality/use_cases.py
+++ app/creative_quality/use_cases.py
@@ -1,6 +1,10 @@
+import gspread
+from django.conf import settings
 from django.db import IntegrityError
+from google.oauth2.service_account import Credentials
 from message_sender.tasks import send_log_message_task
 
+from creative_quality.creative_table import CreativeDto, CreativeGoogleTable
 from creative_quality.models import Creative, CreativeProjectSection, Task
 from creative_quality.services import CreativeProjectSectionService, CreativeService, SendEstimationMessageService
 
@@ -47,6 +51,38 @@ class SendEstimationMessageUseCase:
         for creative in creatives:
             self.estimation_service.send_reminder(creative=creative)
         return {"count": len(creatives)}
+
+
+class SendCreativesToGoogleSheetUseCase:
+    def _convert_creative_to_dto(self, creative: Creative) -> CreativeDto:
+        return CreativeDto(
+            assignee=creative.task.get_assignee_display(),
+            bayer_code=creative.task.bayer_code,
+            hold=creative.hold,
+            hook=creative.hook,
+            ctr=creative.ctr,
+            task_name=creative.task.task_name,
+            task_url=creative.task.task_url,
+            status=creative.status,
+            comment=creative.comment,
+        )
+
+    def execute(self) -> dict:
+        """
+        Raises:
+             gspread.GSpreadException
+        """
+        creatives_to_send = Creative.objects.need_send_to_gsheet().select_related("task")
+        creatives_dto = [self._convert_creative_to_dto(creative=creative) for creative in creatives_to_send]
+        scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+        creds = Credentials.from_service_account_file(
+            settings.GOOGLE_CREDENTIALS_PATH,
+            scopes=scopes,
+        )
+        client = gspread.authorize(credentials=creds)
+        google_table = CreativeGoogleTable(client=client)
+        result = google_table.add_creatives(creatives=creatives_dto)
+        return dict(result)
 
 
 class FetchMissingTasksUseCase:
