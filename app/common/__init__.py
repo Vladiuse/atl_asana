c6192556c213e3c6f898a4ff6e2befbe793aa7f9
vladiuse <vladiuse@gmail.com>
2025-12-16 17:47:42 +0300

    add init

---
+++ app/common/__init__.py
@@ -0,0 +1,10 @@
+from .landing_table_sender import TableSender
+from .message_sender import MessageSender
+from .request_sender import RequestsSender
+
+
+__all__ = [
+    "TableSender",
+    "MessageSender",
+    "RequestsSender",
+]
