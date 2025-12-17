a47e9140ca9f142f64e2e4022a851dbfafdc458d
vladiuse <vladiuse@gmail.com>
2025-12-17 14:37:22 +0300

    update utrls

--- app/creative_quality/urls.py
+++ app/creative_quality/urls.py
@@ -7,13 +7,23 @@ app_name = "creative_quality"
 
 urlpatterns = [
     path(
-        "estimate-creative/<int:creative_id>/<str:task_id>/",
-        views.CreativeUpdateView.as_view(),
-        name="update-creative",
+        "creative/<int:creative_pk>/mark_estimate/",
+        views.CreativeMarkEstimateView.as_view(),
+        name="creative_mark_estimate",
     ),
     path(
         "creative/<int:creative_id>/<str:task_id>/",
-         views.CreativeDetailView.as_view(),
+        views.CreativeDetailView.as_view(),
         name="creative_detail",
+    ),
+    path(
+        "creative-geo-data/<int:geo_data_pk>/",
+        views.CreativeGeoDataView.as_view(),
+        name="creative_geo_data",
+    ),
+    path(
+        "creative-geo-data/<int:creative_pk>/create-geo-data/",
+        views.CreativeGeoDataCreateView.as_view(),
+        name="creative_geo_data_create",
     ),
 ]
