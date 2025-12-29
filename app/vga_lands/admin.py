e6cd231193c49368f126113632271dabf10726ce
vladiuse <vladiuse@gmail.com>
2025-12-29 16:47:05 +0300

    fix

--- app/vga_lands/admin.py
+++ app/vga_lands/admin.py
@@ -6,29 +6,27 @@ from django.contrib import admin
 from .models import AsanaProject, AsanaWebhookRequestData, CompletedTask
 
 
+@admin.register(AsanaProject)
 class AsanaProjectAdmin(admin.ModelAdmin):
-    list_display = ["name", "complete_section_id", "secret", "table_url"]
-    list_display_links = ["name"]
+    list_display = ("name", "complete_section_id", "secret", "table_url")
+    list_display_links = ("name",)
 
 
+@admin.register(AsanaWebhookRequestData)
 class AsanaWebhookRequestDataAdmin(admin.ModelAdmin):
-    list_display = ["id", "__str__", "is_target_event", "project__name", "created"]
-    list_display_links = ["id", "__str__"]
+    list_display = ("id", "__str__", "is_target_event", "project__name", "created")
+    list_display_links = ("id", "__str__")
 
 
+@admin.register(CompletedTask)
 class CompletedTaskAdmin(admin.ModelAdmin):
-    list_display = ["id", "task_id", "project__name", "is_send_in_table", "to_status", "created"]
-    list_display_links = ["id", "task_id"]
+    list_display = ("id", "task_id", "project__name", "is_send_in_table", "to_status", "created")
+    list_display_links = ("id", "task_id")
 
     @admin.display(boolean=True, description="To")
-    def to_status(self, obj) -> bool | None:
+    def to_status(self, obj: CompletedTask) -> bool | None:
         try:
             data = json.loads(obj.response_text)
             return data.get("success")
         except JSONDecodeError:
             return None
-
-
-admin.site.register(AsanaProject, AsanaProjectAdmin)
-admin.site.register(AsanaWebhookRequestData, AsanaWebhookRequestDataAdmin)
-admin.site.register(CompletedTask, CompletedTaskAdmin)
