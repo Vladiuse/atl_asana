4ccb811d2e94c1c48013ed0c12330ec2bdb2d739
vladiuse <vladiuse@gmail.com>
2025-12-29 16:43:12 +0300

    fix

---
+++ app/x.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from functools import cached_property
+
+
+@dataclass
+class A:
+    some: str | None = None
+
+
+def test(x: str) -> str:
+    return x
+
+
+a = A(some=None)
+test(x=a.some)
+
+
+class Test:
+    def __str__(self) -> str:
+        return ""
+
+    @cached_property
+    def test(self) -> None:
+        pass
+
