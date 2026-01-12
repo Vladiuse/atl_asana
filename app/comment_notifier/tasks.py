453784896beacf414ce5550496589e0b629eb65c
vladiuse <vladiuse@gmail.com>
2026-01-12 14:08:15 +0300

    refactor

--- app/comment_notifier/tasks.py
+++ app/comment_notifier/tasks.py
@@ -1,7 +1,7 @@
 from dataclasses import asdict
 
 from asana.client import AsanaApiClient
-from celery import shared_task
+from celery import Task, shared_task
 from common import MessageSender, RequestsSender
 from django.conf import settings
 
@@ -17,7 +17,7 @@ message_sender = MessageSender(request_sender=RequestsSender())
 
 
 @shared_task(bind=True, max_retries=2, default_retry_delay=60 * 5)
-def notify_new_asana_comments_tasks(self, comment_id: str) -> None:
+def notify_new_asana_comments_tasks(self: Task, comment_id: str) -> None:  # type: ignore[type-arg]
     try:
         use_case = AsanaCommentNotifierUseCase(asana_api_client=asana_api_client, message_sender=message_sender)
         return use_case.execute(comment_id=comment_id)
@@ -26,28 +26,33 @@ def notify_new_asana_comments_tasks(self, comment_id: str) -> None:
 
 
 @shared_task(bind=True, max_retries=2, default_retry_delay=15)
-def process_asana_new_comments_task(self, asana_webhook_id: int) -> dict | None:
+def process_asana_new_comments_task(self: Task, asana_webhook_id: int) -> dict | None:  # type: ignore[type-arg]
     try:
         asana_webhook = AsanaWebhookRequestData.objects.get(pk=asana_webhook_id)
         result = ProcessAsanaNewCommentEvent().process(asana_webhook)
         for asana_new_comment_event in result.comments:
-            notify_new_asana_comments_tasks.apply_async(args=[asana_new_comment_event.comment_id], countdown=60)
+            notify_new_asana_comments_tasks.apply_async(  # type: ignore[attr-defined]
+                args=[asana_new_comment_event.comment_id],
+                countdown=60,
+            )
         return asdict(result)
     except Exception as error:  # noqa: BLE001
         self.retry(exc=error)
+        return None
 
 
 @shared_task(bind=True, max_retries=1, default_retry_delay=60 * 3)
-def fetch_missing_project_comments_task(self, send_messages: bool = True) -> dict | None:
+def fetch_missing_project_comments_task(self: Task, *, send_messages: bool = True) -> dict | None:  # type: ignore[type-arg]
     try:
         use_case = FetchMissingProjectCommentsUseCase(asana_api_client=asana_api_client)
         return use_case.execute(send_messages=send_messages)
     except Exception as error:  # noqa: BLE001
         self.retry(exc=error)
+        return None
 
 
 @shared_task()
-def fetch_comment_tasks_urls_task() -> dict:
+def fetch_comment_tasks_urls_task() -> dict[str, int]:
     queryset = AsanaComment.objects.all()
     use_case = LoadCommentsAdditionalInfo(asana_api_client=asana_api_client)
     return use_case.load(queryset=queryset)
