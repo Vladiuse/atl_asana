aa4f9dc5de22af4a716a1a2e5421ab78a2854baf
vladiuse <vladiuse@gmail.com>
2026-01-12 14:21:40 +0300

    refctor

--- app/comment_notifier/use_cases.py
+++ app/comment_notifier/use_cases.py
@@ -20,12 +20,15 @@ class AsanaCommentNotifierUseCase:
     def execute(self, comment_id: str) -> None:
         logging.info("AsanaCommentNotifier comment_id: %s", comment_id)
         comment_model = AsanaComment.objects.get(comment_id=comment_id)
-        project: AsanaWebhookProject = comment_model.project
+        project: AsanaWebhookProject | None = comment_model.project
+        if project is None:
+            msg = "Project is missing"
+            raise ValueError(msg)
         if project.message_sender is None:
             from message_sender.tasks import send_log_message_task
 
             message = f"🛑 {self.__class__.__name__}\nSet {ProjectNotifySender.__name__} for project: {project}"
-            send_log_message_task.delay(message=message)
+            send_log_message_task.delay(message=message) # type: ignore[attr-defined]
             raise NoSenderClassInProjectError(message)
         project_message_sender = SENDERS_REGISTRY[project.message_sender.name].sender
         comment_notifier = AsanaCommentNotifier(
@@ -40,7 +43,7 @@ class AsanaCommentNotifierUseCase:
 class FetchMissingProjectCommentsUseCase:
     asana_api_client: AsanaApiClient
 
-    def execute(self, *,send_messages: bool = True) -> dict:
+    def execute(self, *,send_messages: bool = True) -> dict[str, int]:
         from .tasks import notify_new_asana_comments_tasks
 
         """
@@ -69,7 +72,7 @@ class FetchMissingProjectCommentsUseCase:
                         )
                         project_comments_count += 1
                         if send_messages:
-                            notify_new_asana_comments_tasks.apply_async(
+                            notify_new_asana_comments_tasks.apply_async( # type: ignore[attr-defined]
                                 args=[comment_data["comment_id"]],
                                 countdown=60,
                             )
@@ -79,11 +82,11 @@ class FetchMissingProjectCommentsUseCase:
                         message = (
                             f"⚠️ {self.__class__.__name__}\nCant save asana comment: {comment_data}\n{error}"
                         )
-                        send_log_message_task.delay(message=message)
+                        send_log_message_task.delay(message=message) # type: ignore[attr-defined]
                         errors_count += 1
             use_case_result[str(project)] = project_comments_count
         if missing_comments_found:
             message = f"⚠️ Missing comments found: {missing_comments_found}"
-            send_log_message_task.delay(message=message)
+            send_log_message_task.delay(message=message) # type: ignore[attr-defined]
         use_case_result["errors_count"] = errors_count
         return use_case_result
