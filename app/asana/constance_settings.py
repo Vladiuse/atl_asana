6879d43a9b6e38fd08759444eaa2d289814d57fc
vladiuse <vladiuse@gmail.com>
2026-01-12 19:01:31 +0300

    add constant

--- app/asana/constance_settings.py
+++ app/asana/constance_settings.py
@@ -1,11 +1,16 @@
 CONSTANCE_CONFIG = {
-    "DESIGN_TASK_BAER_CUSTOM_FIELD_NAME": (
+    "DESIGN_TASK_BAYER_CUSTOM_FIELD_NAME": (
         "Инициалы Баера",
         "Название поля с указанием баера в таске на досках дизайна",
+        str,
+    ),
+    "DESIGN_TASK_LINK_ON_WORK_FIELD_NAME": (
+        "Ссылка на работу",
+        "Название поля с указанием ссылки на работу в таске на досках дизайна",
         str,
     ),
 }
 
 CONSTANCE_CONFIG_FIELDSETS = {
-    "ASANA": ("DESIGN_TASK_BAER_CUSTOM_FIELD_NAME",),
+    "ASANA": ("DESIGN_TASK_BAYER_CUSTOM_FIELD_NAME", "DESIGN_TASK_LINK_ON_WORK_FIELD_NAME"),
 }
