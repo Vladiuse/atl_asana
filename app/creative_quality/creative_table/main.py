5e3836fc8e95b00179e8b488e5f8a2918ade543e
vladiuse <vladiuse@gmail.com>
2026-01-12 13:35:41 +0300

    refactor

--- app/creative_quality/creative_table/main.py
+++ app/creative_quality/creative_table/main.py
@@ -6,18 +6,18 @@ from gspread import Client
 from gspread.worksheet import JSONResponse
 
 """
-EXAMPLE how to connet to google sheets
+EXAMPLE how to connect to google sheets
 ~~~~~~~~~~~~~~~~~
 
 import gspread
 from google.oauth2.service_account import Credentials
 
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-creds = Credentials.from_service_account_file(
+credentials = Credentials.from_service_account_file(
     settings.GOOGLE_CREDENTIALS_PATH,
     scopes=scopes,
 )
-client = gspread.authorize(credentials=creds)
+client = gspread.authorize(credentials=credentials)
 sheet = client.open_by_key("1ztb8tw4nlT7Xk8FJAy_I-wXsWAggP_pKudXIDRVvSz8").worksheet(title="Лист1")
 sheet.append_rows(
     values=[
@@ -33,9 +33,9 @@ class CreativeDto:
     country: str
     assignee: str
     bayer_code: str
-    hook: str
-    hold: str
-    ctr: str
+    hook: float
+    hold: float
+    ctr: float
     task_name: str
     task_url: str
     status: str
@@ -46,7 +46,7 @@ class CreativeGoogleTable:
     def __init__(self, client: Client):
         self.client = client
 
-    def _convert_creative_to_line(self, creative_dto: CreativeDto) -> list:
+    def _convert_creative_to_line(self, creative_dto: CreativeDto) -> list[str | int | float]:
         return [
             timezone.localtime().strftime("%Y-%m-%d %H:%M:%S"),
             creative_dto.country,
@@ -62,13 +62,13 @@ class CreativeGoogleTable:
         ]
 
     def add_creatives(self, creatives: list[CreativeDto]) -> JSONResponse:
-        """
+        """Add line with data to table.
+
         Raises:
              GSpreadException
+
         """
-        data_to_send = []
-        for creative in creatives:
-            data_to_send.append(self._convert_creative_to_line(creative_dto=creative))
+        data_to_send = [self._convert_creative_to_line(creative_dto=creative) for creative in creatives]
         sheet = self.client.open_by_key(config.CREATIVE_GOOGLE_TABLE_ID).worksheet(
             title=config.CREATIVE_TABLE_LIST_NAME,
         )
