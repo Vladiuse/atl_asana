8d01d2e1e32b473ca0b515c0ee4093546a49628a
vladiuse <vladiuse@gmail.com>
2026-01-12 23:36:02 +0300

    fix asana version

--- app/asana/utils.py
+++ app/asana/utils.py
@@ -1,2 +1,2 @@
 def get_asana_profile_url_by_id(profile_id: str) -> str:
-    return f"https://app.asana.com/0/profile/{profile_id}"
+    return f"https://app.asana.com/1/profile/{profile_id}"
--- app/comment_notifier/utils.py
+++ app/comment_notifier/utils.py
@@ -1,6 +1,6 @@
 import re
 
-USER_PROFILE_LINK_PATTERN = re.compile(r"https://app\.asana\.com/\d+/profile/(\d+)")
+USER_PROFILE_LINK_PATTERN = re.compile(r"https://app\.asana\.com/(?:1|0)/\d+/profile/(\d+)")
 
 
 def extract_user_profile_id_from_text(text: str) -> list[str]:
