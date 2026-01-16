2604090ddb9c95e1d972918035bbf98d6e299fbe
vladiuse <vladiuse@gmail.com>
2026-01-16 20:51:46 +0300

    add sccript

---
+++ app/message_sender/scripts/message_sender/update_users.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+from message_sender.client import MessageSender
+from message_sender.services import UserService
+
+client = MessageSender(
+    host=settings.MESSAGE_SENDER_HOST,
+    api_key=settings.DOMAIN_MESSAGE_API_KEY,
+)
+
+service = UserService(message_sender_client=client)
+def run() -> None:
+    result = service.update_all_users()
+    print(result)
