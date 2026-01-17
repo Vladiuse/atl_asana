a0bd13e9c9105d322a1055e58209b61a6f0d66eb
vladiuse <vladiuse@gmail.com>
2026-01-17 14:54:51 +0300

    refactor

--- app/message_sender/tasks.py
+++ app/message_sender/tasks.py
@@ -1,9 +1,13 @@
 # mypy: disable-error-code=type-arg
 from celery import Task, shared_task
-from common import MessageSender, RequestsSender
-from common.message_sender import UserTag
+from django.conf import settings
 
-message_sender = MessageSender(request_sender=RequestsSender())
+from message_sender.client import Handlers, MessageSender
+
+message_sender = MessageSender(
+    host=settings.MESSAGE_SENDER_HOST,
+    api_key=settings.DOMAIN_MESSAGE_API_KEY,
+)
 DELAY_RETRY = 60
 
 
@@ -18,14 +22,13 @@ def send_log_message_task(self: Task, message: str) -> None:
 @shared_task(bind=True, max_retries=2, default_retry_delay=DELAY_RETRY)
 def send_message_to_user_task(self: Task, user_tags: list[str], message: str) -> None:
     try:
-        tags = [UserTag(value) for value in user_tags]
-        message_sender.send_message_to_user(user_tags=tags, message=message)
+        message_sender.send_message_to_users(user_tags=user_tags, message=message)
     except Exception as error:  # noqa: BLE001
         self.retry(exc=error)
 
 
 @shared_task(bind=True, max_retries=2, default_retry_delay=DELAY_RETRY)
-def send_message_task(self: Task, handler: str, message: str) -> None:
+def send_message_task(self: Task, handler: Handlers, message: str) -> None:
     try:
         message_sender.send_message(handler=handler, message=message)
     except Exception as error:  # noqa: BLE001
