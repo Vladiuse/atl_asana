e7714f5abb09469f1cb6b53adc663c5d6f6367b3
vladiuse <vladiuse@gmail.com>
2026-02-05 12:42:18 +0300

    add urls for customer

--- app/valentine_day/urls.py
+++ app/valentine_day/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from . import views
+
+router = DefaultRouter()
+router.register("employee", views.CustomerView)
 
 app_name = "valentine_day"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("", include(router.urls)),
 ]
--- app/valentine_day/views.py
+++ app/valentine_day/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Customer
+from .serializers import CustomerSerializer
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "valentine_day/index.html")
+
+
+class CustomerView(viewsets.ModelViewSet):  # type: ignore[type-arg]
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
