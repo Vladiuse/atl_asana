fa43bb09c5630abad1b0ea864483e3cfdf54fe23
vladiuse <vladiuse@gmail.com>
2026-02-05 10:48:03 +0300

    add admin

--- app/valentine_day/admin.py
+++ app/valentine_day/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Customer
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    list_display = ("id", "name", "surname", "position", "image_tag")
+
+    @admin.display(description="Img")
+    def image_tag(self, obj: Customer) -> str:
+        if obj.image:
+            return format_html('<img src="{}" style="height:50px;"/>', obj.image.url)
+        return "-"
