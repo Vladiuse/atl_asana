8e399318e1d6f3e0bcdd5e28402dcc8289bf7476
vladiuse <vladiuse@gmail.com>
2026-02-05 10:28:48 +0300

    add model Cusotomer

--- app/valentine_day/models.py
+++ app/valentine_day/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    image = models.ImageField(
+        upload_to="images/",
+        blank=True,
+        max_length=255,
+    )
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    position = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.surname}"
