2c267a5f76aea79fa07212f70aa044a5b72c02f1
vladiuse <vladiuse@gmail.com>
2026-02-05 12:57:44 +0300

    change fields

--- app/valentine_day/models.py
+++ app/valentine_day/models.py
@@ -4,12 +4,19 @@ from django.db import models
 class Employee(models.Model):
     image = models.ImageField(
         upload_to="valentine_day/images/avatars",
+        null=True,
         blank=True,
         max_length=255,
     )
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    position = models.CharField(max_length=100)
+    name = models.CharField(
+        max_length=50,
+    )
+    surname = models.CharField(
+        max_length=50,
+    )
+    position = models.CharField(
+        max_length=100,
+    )
 
     def __str__(self) -> str:
         return f"{self.name} {self.surname}"
