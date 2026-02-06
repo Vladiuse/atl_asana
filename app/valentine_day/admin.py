74b2377c9233b8d0c36efee2999372072e65cf81
vladiuse <vladiuse@gmail.com>
2026-02-06 21:02:30 +0300

    add models

--- app/valentine_day/admin.py
+++ app/valentine_day/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from .models import Employee
+from .models import Employee, Valentine, ValentineImage
 
 
 @admin.register(Employee)
@@ -12,4 +12,71 @@ class CustomerAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
     def image_tag(self, obj: Employee) -> str:
         if obj.image:
             return format_html('<img src="{}" style="height:60px;"/>', obj.image.url)
+        return "-"
+
+
+@admin.register(ValentineImage)
+class ValentineImageAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    list_display = ("id", "owner", "image_tag", "created")
+    list_filter = ("owner", "created")
+    search_fields = ("owner__full_name",)
+    readonly_fields = ("created",)
+
+    @admin.display(description="Img")
+    def image_tag(self, obj: ValentineImage) -> str:
+        if obj.image:
+            return format_html('<img src="{}" style="height:60px;"/>', obj.image.url)
+        return "-"
+
+
+@admin.register(Valentine)
+class ValentineAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    list_display = (
+        "id",
+        "sender",
+        "recipient",
+        "image_tag",
+        "is_anonymously",
+        "created",
+    )
+    list_filter = ("recipient",)
+    search_fields = (
+        "sender__full_name",
+        "recipient__full_name",
+    )
+    readonly_fields = ("created",)
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "sender",
+                    "recipient",
+                    "image",
+                    "text",
+                ),
+            },
+        ),
+        (
+            "Анонимность",
+            {
+                "fields": (
+                    "is_anonymously",
+                    "anonymous_signature",
+                ),
+            },
+        ),
+        (
+            "Системные поля",
+            {
+                "fields": ("created",),
+            },
+        ),
+    )
+
+    @admin.display(description="Img")
+    def image_tag(self, obj: Valentine) -> str:
+        if obj.image.image:
+            return format_html('<img src="{}" style="height:60px;"/>', obj.image.image.url)
         return "-"
--- app/valentine_day/models.py
+++ app/valentine_day/models.py
@@ -1,4 +1,8 @@
+from typing import Any
+
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Q
 
 
 class Employee(models.Model):
@@ -17,6 +21,77 @@ class Employee(models.Model):
     position = models.CharField(
         max_length=100,
     )
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
 
     def __str__(self) -> str:
         return f"{self.name} {self.surname}"
+
+
+class ValentineImage(models.Model):
+    image = models.ImageField(
+        upload_to="valentine_day/images/for_valentines",
+        null=True,
+        blank=True,
+        max_length=255,
+    )
+    owner = models.ForeignKey(
+        to=Employee,
+        on_delete=models.CASCADE,
+        null=True,
+        default=None,
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self) -> str:
+        return f"<ValentineImage: {self.pk}>"
+
+
+class Valentine(models.Model):
+    sender = models.ForeignKey(
+        to=Employee,
+        on_delete=models.CASCADE,
+        related_name="sent_valentines",
+    )
+    recipient = models.ForeignKey(
+        to=Employee,
+        on_delete=models.CASCADE,
+        related_name="received_valentines",
+    )
+    image = models.ForeignKey(
+        to=ValentineImage,
+        on_delete=models.PROTECT,
+    )
+    text = models.TextField()
+    is_anonymously = models.BooleanField()
+    anonymous_signature = models.TextField(
+        max_length=50,
+        blank=True,
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        unique_together = ("sender", "recipient")
+        constraints = (
+            models.CheckConstraint(
+                name="anonymous_requires_signature",
+                condition=(Q(is_anonymously=False) | Q(anonymous_signature__gt="")),
+            ),
+        )
+
+    def __str__(self) -> str:
+        return f"{self.sender} to {self.recipient}"
+
+    def save(self, *args: Any, **kwargs: Any) -> None:  # noqa: ANN401
+        if not self.is_anonymously:
+            self.anonymous_signature = ""
+        super().save(*args, **kwargs)
+
+    def clean(self) -> None:
+        if self.is_anonymously and self.anonymous_signature == "":
+            raise ValidationError("При анонимной отправке нужно указать подпись")
