9fb6275d9b0ad48fd88eff4cc7439618d03b6157
vladiuse <vladiuse@gmail.com>
2026-02-06 21:05:44 +0300

    add serialziers

--- app/valentine_day/serializers.py
+++ app/valentine_day/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 
-from .models import Employee
+from .models import Employee, Valentine, ValentineImage
 
 
 class CustomerSerializer(serializers.ModelSerializer):  # type: ignore[type-arg]
-
     class Meta:
         model = Employee
         fields = "__all__"
 
+
+class ValentineImageSerializer(serializers.ModelSerializer):  # type: ignore[type-arg]
+    class Meta:
+        models = ValentineImage
+        fields = "__all__"
+
+
+class ValentineSerializer(serializers.ModelSerializer):  # type: ignore[type-arg]
+    class Meta:
+        model = Valentine
+        fields = "__all__"
