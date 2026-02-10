bfb9899b57bb7bb5a97524758d6a39ae614f6718
vladiuse <vladiuse@gmail.com>
2026-02-10 13:58:45 +0300

    add update

--- app/valentine_day/constance_settings.py
+++ app/valentine_day/constance_settings.py
@@ -1,13 +1,13 @@
+from datetime import datetime
+
 CONSTANCE_CONFIG = {
     "SHOW_VALENTINES_AT": (
-        None,
+        datetime(2026, 2, 13, 14, 0, 0),  # noqa: DTZ001
         "В какое время открыть полученные валентинки",
         "datetime",
     ),
 }
 
 CONSTANCE_CONFIG_FIELDSETS = {
-    "VALENTINE_DAY": (
-        "SHOW_VALENTINES_AT",
-    ),
+    "VALENTINE_DAY": ("SHOW_VALENTINES_AT",),
 }
