4f7cd41487cde2abfd95bcdb8c93350a68239305
vladiuse <vladiuse@gmail.com>
2026-02-14 17:55:44 +0300

    add test

--- app/leave_events/tests/test_services.py
+++ app/leave_events/tests/test_services.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 import pytest
+from django.utils import timezone
 from message_sender.models import ScheduledMessage
 
 from leave_events.models import Leave, LeaveStatus, LeaveType
@@ -29,16 +30,29 @@ class TestLeaveServicePending:
         assert ScheduledMessage.objects.filter(reference_id=f"leave-{leave.pk}").count() == 1
 
     def test_exist_in_db(self, service: LeaveNotificationService) -> None:
+        """Leave already exist in db. Must be removed old messages."""
         leave_data = {
             "employee": "xxx",
             "supervisor_tag": "xxx",
             "start_date": date(2000, 1, 1),
             "end_date": date(2000, 1, 1),
             "type": LeaveType.DAY_OFF.value,
-            "status": LeaveStatus.PENDING.value,
+            "status": LeaveStatus.APPROVED.value,
         }
         leave_orig = Leave.objects.create(**leave_data)
+        for _ in range(2):
+            ScheduledMessage.objects.create(
+                text="x",
+                user_tag="x",
+                run_at=timezone.now(),
+                reference_id=f"leave-{leave_orig.pk}",
+            )
+        assert leave_orig.messages.count() == 2
+        assert leave_orig.status == LeaveStatus.APPROVED.value
+        leave_data["status"] = LeaveStatus.PENDING.value
         leave = service._need_agreed(leave_data=leave_data)
         assert Leave.objects.count() == 1
         assert ScheduledMessage.objects.filter(reference_id=f"leave-{leave.pk}").count() == 1
         assert leave_orig.pk == leave.pk
+        assert leave.status == LeaveStatus.PENDING.value
+        assert leave.messages.count() == 1
