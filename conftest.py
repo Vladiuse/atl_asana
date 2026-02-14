e069e4fe55af8a3b8278fa13b285fe65fa4e448a
vladiuse <vladiuse@gmail.com>
2026-02-14 19:27:32 +0300

    update conf test

--- conftest.py
+++ conftest.py
@@ -1,5 +1,12 @@
+import pytest
 from pytest_socket import disable_socket
+from rest_framework.test import APIClient
 
 
 def pytest_runtest_setup() -> None:
     disable_socket()
+
+
+@pytest.fixture
+def api_client() -> APIClient:
+    return APIClient()
