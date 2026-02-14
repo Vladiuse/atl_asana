59403ee1e3e96969ae041214eac09be1d6afe6e9
vladiuse <vladiuse@gmail.com>
2026-02-14 13:57:30 +0300

    add const

--- app/leave_events/constance_settings.py
+++ app/leave_events/constance_settings.py
@@ -9,6 +9,11 @@ CONSTANCE_CONFIG = {
         "За сколько дней до отпуска оповещать об этом",
         int,
     ),
+    "LEAVE_TABLE_URL": (
+        str,
+        "Ссылка на табличку с отпусками",
+        "https://docs.google.com/spreadsheets/d/1bbo6WxBLGk24FeSRucCYkwuWu1cYafb_5XsVgBO1DnY/edit?gid=570923352#gid=570923352",
+    ),
 }
 
 CONSTANCE_CONFIG_FIELDSETS = {
