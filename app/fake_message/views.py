63dbcbff3d09008442644d4fbdcb641cb2ede554
vladiuse <vladiuse@gmail.com>
2026-03-05 21:49:49 +0300

    add filer

--- app/fake_message/views.py
+++ app/fake_message/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from .models import Message
@@ -7,3 +8,5 @@ from .serializers import MessageSerializer
 class MessageView(viewsets.ModelViewSet):  # type: ignore[type-arg]
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ("status", "tag")
