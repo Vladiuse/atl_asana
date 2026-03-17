51a3fa93c21b66af6266029da54b5ad56690b494
vladiuse <vladiuse@gmail.com>
2026-03-17 12:44:04 +0300

    refactor

--- app/asana/use_cases.py
+++ app/asana/use_cases.py
@@ -1,6 +1,10 @@
+import logging
+
 from message_sender.tasks import send_log_message_task
 
 from asana.repository import AsanaUserRepository
+
+logger = logging.getLogger()
 
 
 class FetchNewAsanaUsers:
@@ -9,7 +13,8 @@ class FetchNewAsanaUsers:
 
     def execute(self) -> dict[str, int]:
         result = self.asana_users_repository.update_all()
+        logger.info("Fetch new asana users: %s", result)
         if result["new_created"] > 0:
-            message = f"Find new asana users: {result['new_created']}"
+            message = f"ℹ️ Find new asana users: {result['new_created']}"
             send_log_message_task.delay(message=message)  # type: ignore[attr-defined]
         return result
