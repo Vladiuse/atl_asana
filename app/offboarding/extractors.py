6760c441a711a4e2e44beafc3aef757a542e37a0
vladiuse <vladiuse@gmail.com>
2026-03-18 20:38:43 +0300

    fix error

--- app/offboarding/extractors.py
+++ app/offboarding/extractors.py
@@ -14,11 +14,12 @@ def extract_offboarding_task_data(task_data: dict[str, Any]) -> TaskData:
 
     """
     fio = get_field_value_from_task(field_name="ФИО", task_data=task_data)
-    tag = get_field_value_from_task(field_name="ТЭГ", task_data=task_data)
+    tag = get_field_value_from_task(field_name="ТЕГ", task_data=task_data)
     position = get_field_value_from_task(field_name="Должность", task_data=task_data)
     url = task_data["permalink_url"]
     if not all([fio, tag, position]):
-        raise OffboardingAppError("Not all required fields are filled")
+        missing_fields = [name for name, value in (("fio", fio), ("tag", tag), ("position", position)) if not value]
+        raise OffboardingAppError(f"Not all required fields are filled: missing {', '.join(missing_fields)}")
     assert fio is not None  #  noqa: S101
     assert tag is not None  #  noqa: S101
     assert position is not None  #  noqa: S101
