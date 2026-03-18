09b02909a91746ed53e9a544dd76a06001ab31ba
vladiuse <vladiuse@gmail.com>
2026-03-18 18:18:14 +0300

    add few tests

---
+++ app/offboarding/tests/fixtures_data.py
@@ -0,0 +1,32 @@
+RESOURCE_ID = "1234567890987654321"
+
+CREATE_TASK_EVENT = {
+    "events": [
+        {
+            "user": {"gid": "1213537811762722", "resource_type": "user"},
+            "action": "added",
+            "parent": {"gid": "1210393628043121", "resource_type": "project"},
+            "resource": {"gid": RESOURCE_ID, "resource_type": "task", "resource_subtype": "default_task"},
+            "created_at": "2026-03-17T10:11:06.474Z",
+        },
+        {
+            "user": {"gid": "1213537811762722", "resource_type": "user"},
+            "action": "added",
+            "parent": {"gid": "1211316915894850", "resource_type": "section"},
+            "resource": {"gid": RESOURCE_ID, "resource_type": "task", "resource_subtype": "default_task"},
+            "created_at": "2026-03-17T10:11:06.611Z",
+        },
+    ],
+}
+
+CREATE_SUB_TASK_EVENT = {
+    "events": [
+        {
+            "user": {"gid": "1213537811762722", "resource_type": "user"},
+            "action": "added",
+            "parent": {"gid": "1213717492740873", "resource_type": "task", "resource_subtype": "default_task"},
+            "resource": {"gid": RESOURCE_ID, "resource_type": "task", "resource_subtype": "default_task"},
+            "created_at": "2026-03-17T10:11:18.514Z",
+        },
+    ],
+}
---
+++ app/offboarding/tests/services/test_create_service.py
@@ -0,0 +1,40 @@
+from typing import Any
+from unittest.mock import Mock
+
+import pytest
+from asana.models import AsanaWebhookRequestData
+
+from offboarding.models import OffboardingTask
+from offboarding.services import OffboardingTaskCreateService
+from offboarding.tests.fixtures_data import CREATE_SUB_TASK_EVENT, CREATE_TASK_EVENT, RESOURCE_ID
+
+NOT_TARGET_RESOURCE = {
+    "events": [
+        {
+            "user": {"gid": "1213537811762722", "resource_type": "user"},
+            "action": "XXX",
+            "parent": {"gid": "1213717492740873", "resource_type": "task", "resource_subtype": "default_task"},
+            "resource": {"gid": "1213717492740875", "resource_type": "XXX", "resource_subtype": "default_task"},
+            "created_at": "2026-03-17T10:11:18.514Z",
+        },
+    ]
+}
+
+@pytest.mark.django_db
+class TestCreateService:
+    @pytest.mark.parametrize(
+        ("event_data", "is_must_create"),
+        [
+            (CREATE_TASK_EVENT, True),
+            (CREATE_SUB_TASK_EVENT, False),
+            (NOT_TARGET_RESOURCE, False),
+        ],
+    )
+    def test_create_from_webhook_data(self, event_data: dict[str, Any], is_must_create: bool) -> None:
+        service = OffboardingTaskCreateService()
+        mock_webhook = Mock(spec=AsanaWebhookRequestData)
+        mock_webhook.payload = event_data
+        service.create_from_webhook(webhook_data=mock_webhook)
+        assert OffboardingTask.objects.exists() == is_must_create
+        if is_must_create:
+            OffboardingTask.objects.filter(asana_task_id=RESOURCE_ID).exists()
