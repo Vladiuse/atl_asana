2df8e65352eed2540d5a0bec83ad0536ec358e4d
vladiuse <vladiuse@gmail.com>
2026-03-18 12:56:07 +0300

    add util get_field_value_from_task

--- app/asana/utils.py
+++ app/asana/utils.py
@@ -1,2 +1,23 @@
+from typing import Any
+
+from .exception import FieldNotFoundError
+
+
 def get_asana_profile_url_by_id(profile_id: str, workspace_id: str) -> str:
     return f"https://app.asana.com/1/{workspace_id}/profile/{profile_id}"
+
+
+def get_field_value_from_task(
+    field_name: str,
+    task_data: dict[str, Any],
+    default_value: str | None = None,
+    *,
+    raise_if_not_found: bool = False,
+) -> str | None:
+    for custom_field in task_data["custom_fields"]:
+        if field_name == custom_field["name"]:
+            return custom_field["text_value"]
+    if raise_if_not_found:
+        msg = f'Field "{field_name}" not found in asana task data'
+        raise FieldNotFoundError(msg)
+    return default_value
