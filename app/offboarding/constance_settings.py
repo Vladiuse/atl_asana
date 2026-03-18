5375e646234fadca40dfbb389fe0053c20810841
vladiuse <vladiuse@gmail.com>
2026-03-18 15:28:12 +0300

    add constarnce

--- app/offboarding/constance_settings.py
+++ app/offboarding/constance_settings.py
@@ -4,8 +4,16 @@ CONSTANCE_CONFIG = {
         "Через сколько поля в каточке оффбординга после создания  будут заполнены (в минутах)",
         int,
     ),
+    "OFFBOARDING_CE_SECTION_ID": (
+        "1205707970532975",
+        'Айди секции "Done" на доске оффбординга',
+        str,
+    ),
 }
 
 CONSTANCE_CONFIG_FIELDSETS = {
-    "OFFBOARDING": ("DELAY_FOR_FEED_CARD",),
+    "OFFBOARDING": (
+        "DELAY_FOR_FEED_CARD",
+        "OFFBOARDING_CE_SECTION_ID",
+    ),
 }
