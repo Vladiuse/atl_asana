92e5d91b68e7b0f521c3ee0ddfdf3af25f345105
vladiuse <vladiuse@gmail.com>
2026-04-07 18:08:50 +0300

    change tests for serializator

--- app/leave_events/tests/test_serializers.py
+++ app/leave_events/tests/test_serializers.py
@@ -2,10 +2,11 @@ import pytest
 from message_sender.models import AtlasUser
 from rest_framework.exceptions import ValidationError
 
+from leave_events.models import LeaveStatus, SupervisorNotificationChat
 from leave_events.serializers import LeaveSerializer
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def atlas_user() -> AtlasUser:
     return AtlasUser.objects.create(
         name="Test User",
@@ -17,11 +18,62 @@ def atlas_user() -> AtlasUser:
     )
 
 
+@pytest.fixture
+def supervisor_chat(atlas_user: AtlasUser) -> SupervisorNotificationChat:
+    return SupervisorNotificationChat.objects.create(supervisor=atlas_user, chat="CHAT")
+
+
 @pytest.mark.django_db
-def test_validate_telegram_login() -> None:
-    serializer = LeaveSerializer()
-    assert serializer.validate_telegram_login("test_telegram_login") == "test_telegram_login"
-    assert serializer.validate_telegram_login("@test_telegram_login") == "test_telegram_login"
-    with pytest.raises(ValidationError) as exc_info:
-        serializer.validate_telegram_login("wrong_telegram")
-    assert "telegram_login" in exc_info.value.detail
+class TestLeaveSerializer:
+    def test_to_internal_value(self, supervisor_chat: SupervisorNotificationChat) -> None:
+        _ = supervisor_chat
+        data = {
+            "telegram_login": "@test_telegram_login",
+            "supervisor_tag": "@test_telegram_login",
+            "employee": "Ivan Ivanov",
+            "type": "VACATION",
+            "start_date": "2026-04-10",
+            "end_date": "2026-04-15",
+            "status": LeaveStatus.PENDING.value,
+        }
+        serializer = LeaveSerializer()
+        result = serializer.to_internal_value(data)
+        assert result["telegram_login"] == "test_telegram_login"
+        assert result["supervisor_tag"] == "test_telegram_login"
+
+    def test_unknown_employee_super_visor(self, supervisor_chat: SupervisorNotificationChat) -> None:
+        _ = supervisor_chat
+        data = {
+            "telegram_login": "@xxx",
+            "supervisor_tag": "@yyy",
+            "employee": "Ivan Ivanov",
+            "type": "VACATION",
+            "start_date": "2026-04-10",
+            "end_date": "2026-04-15",
+            "status": LeaveStatus.PENDING.value,
+        }
+        serializer = LeaveSerializer(data=data)
+        with pytest.raises(ValidationError):
+            serializer.is_valid(raise_exception=True)
+        assert "telegram_login" in serializer.errors
+        assert "supervisor_tag" in serializer.errors
+
+    def test_supervisor_chat_not_exist(self, supervisor_chat: SupervisorNotificationChat) -> None:
+        supervisor_chat.delete()
+        data = {
+            "telegram_login": "@test_telegram_login",
+            "supervisor_tag": "@test_telegram_login",
+            "employee": "Ivan Ivanov",
+            "type": "VACATION",
+            "start_date": "2026-04-10",
+            "end_date": "2026-04-15",
+            "status": LeaveStatus.PENDING.value,
+        }
+        serializer = LeaveSerializer(data=data)
+        with pytest.raises(ValidationError):
+            serializer.is_valid(raise_exception=True)
+        assert "supervisor_tag" in serializer.errors
+        assert (
+            serializer.errors["supervisor_tag"]["supervisor_tag"]
+            == "Supervisor @test_telegram_login dont have chat to notify"
+        )
